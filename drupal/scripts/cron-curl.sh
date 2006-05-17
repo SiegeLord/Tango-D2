d3ea2f549040b0b30ba1065ee06618787ee7c886
brad <brad@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2006-05-17 03:49:50 +0000

    shouldn't store drupal in the actual Vitamin repos, moved it to dsource repos

--- drupal/scripts/cron-curl.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# $Id: cron-curl.sh,v 1.2 2005/08/11 13:02:08 dries Exp $
-
-curl --silent --compressed http://yoursite.com/cron.php
--- drupal/themes/bluemarine/box.tpl.php
+++
@@ -1,5 +0,0 @@
-  <div class="box">
-    <?php if ($title) { ?><h2 class="title"><?php print $title; ?></h2><?php } ?>
-    <div class="content"><?php print $content; ?></div>
- </div>
-
