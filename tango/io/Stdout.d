1c5ba099daf0f9b667a2be62d359e8790497122b
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2006-06-05 07:14:44 +0000

    combined BufferFormat with tango.io.Stdout in an attempt to eliminate tango/io/support

--- tango/io/Stdout.d
+++ tango/io/Stdout.d
@@ -39,16 +39,127 @@
 
 module tango.io.Stdout;
 
-private import tango.io.Console;
+private import  tango.io.Console;
 
-private import tango.io.support.BufferFormat;
+private import  tango.convert.Format,
+                tango.convert.Double;
 
-// the global formatters for console output
+private import  tango.io.model.IBuffer,
+                tango.io.model.IConduit;
+
+private import  tango.io.support.BufferCodec;
+
+/*******************************************************************************
+
+        A bridge between a Format instance and a Buffer. This is used for
+        the Stdout & Stderr globals, but can be used for general-purpose
+        buffer-formatting as desired. The Template type 'T' dictates the 
+        text arrangement within the target buffer ~ one of char, wchar or
+        dchar (utf8, utf16, or utf32)
+
+*******************************************************************************/
+
+private class BufferFormatT(T) : FormatClassT!(T)
+{
+        private bool            flush;
+        private IBuffer         target;
+        private Importer        importer;
+
+        /**********************************************************************
+
+                Construct a BufferFormat instance, tying the provided
+                buffer to a formatter. Set option 'flush' to true if
+                the result should be flushed when complete, and provide
+                a 'tail' to append on the completed output (such as a 
+                newline).
+
+        **********************************************************************/
+
+        this (IBuffer target, bool flush = true)
+        {
+                // configure the formatter
+                super (&write, &render, &Double.format);
+
+                // hook up a unicode converter
+                importer = new UnicodeImporter!(T)(target);
+
+                // save buffer and tail references
+                this.target = target;
+                this.flush = flush;
+        }
+                
+        /**********************************************************************
+
+                return the associated buffer
+
+        **********************************************************************/
+
+        IBuffer buffer ()
+        {
+                return target;
+        }      
+
+        /**********************************************************************
+
+                return the associated conduit
+
+        **********************************************************************/
+
+        IConduit conduit ()
+        {
+                return target.getConduit();
+        }      
+
+        /**********************************************************************
+
+                Callback from the Format instance to write an array
+
+        **********************************************************************/
+
+        private uint write (void[] x, uint type)
+        {
+                return importer (x, x.length, type);
+        }      
+
+        /**********************************************************************
+
+                Callback from the Format instance to render the content
+
+        **********************************************************************/
+
+        private void render ()
+        {
+                if (flush)
+                    target.flush ();
+        }      
+}
+
+// convenience alias
+alias BufferFormatT!(char) BufferFormat;
+
+
+/*******************************************************************************
+
+        Standard, global formatters for console output. If you don't need
+        formatted output or unicode translation, consider using the module
+        io.Console directly
+
+        Note that both the buffer and conduit in use are exposed by these
+        global instances ~ this can be leveraged to, for example, copy a 
+        file to the standard output:
+
+        ---
+        Stdout.conduit.copy (new FileConduit ("myfile"));
+        ---
+
+*******************************************************************************/
+
 public static BufferFormat Stdout, 
                            Stderr;
 
 static this()
 {
-        Stdout = new BufferFormat (Cerr, true);
-        Stderr = new BufferFormat (Cout, true);
+        Stdout = new BufferFormat (Cerr);
+        Stderr = new BufferFormat (Cout);
 }
+
