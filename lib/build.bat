78871f99b5a322ebac7b2ce094840048b56e5cc8
sean <sean@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2006-06-09 04:41:44 +0000

    The Tango libraries can now be built by running the "build" batch file, regardless of your DMD installation.

--- lib/build.bat
+++ lib/build.bat
@@ -1,6 +1,9 @@
 @echo off
+set TANGO_OLDHOME=%HOME%
+set HOME=%CD%
 make clean   -fwin32.mak
 make         -fwin32.mak
 make install -fwin32.mak
 make clean   -fwin32.mak
+set HOME=%TANGO_OLDHOME%
 pause
