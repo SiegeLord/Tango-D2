844e78e9df4aab3a3712791681db76f0bcd07dad
sean <sean@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2006-06-15 18:42:06 +0000

    Added new standard module header.

--- tango/core/vararg.d
+++ tango/core/vararg.d
@@ -1,30 +1,44 @@
-/*
- * Placed in public domain
- * Written by Hauke Duden and Walter Bright
- *
- * Documentation written by Sean Kelly <sean@f4.ca>
- */
-
 /**
  * The vararg module is intended to facilitate vararg manipulation in D.
  * It should be interface compatible with the C module "stdarg," and the
  * two modules may share a common implementation if possible (as is done
  * here).
+ *
+ * Copyright: Public Domain
+ * License:   Public Domain
+ * Authors:   Hauke Duden, Walter Bright
  */
 module tango.core.vararg;
 
 
+/**
+ *
+ */
 alias void* va_list;
 
 
-/**
- *
- */
-template va_start(T)
+template va_start( T )
 {
-    void va_start(out va_list ap, inout T parmn)
+    /**
+     *
+     */
+    void va_start( out va_list ap, inout T parmn )
     {
-	ap = cast(va_list)(cast(void*)&parmn + ((T.sizeof + int.sizeof - 1) & ~(int.sizeof - 1)));
+	    ap = cast(va_list) ( cast(void*) &parmn + ( ( T.sizeof + int.sizeof - 1 ) & ~( int.sizeof - 1 ) ) );
+    }
+}
+
+
+template va_arg( T )
+{
+    /**
+     *
+     */
+    T va_arg( inout va_list ap )
+    {
+	    T arg = *cast(T*) ap;
+	    ap = cast(va_list) ( cast(void*) ap + ( ( T.sizeof + int.sizeof - 1 ) & ~( int.sizeof - 1 ) ) );
+	    return arg;
     }
 }
 
@@ -32,21 +46,7 @@ template va_start(T)
 /**
  *
  */
-template va_arg(T)
-{
-    T va_arg(inout va_list ap)
-    {
-	T arg = *cast(T*)ap;
-	ap = cast(va_list)(cast(void*)ap + ((T.sizeof + int.sizeof - 1) & ~(int.sizeof - 1)));
-	return arg;
-    }
-}
-
-
-/**
- *
- */
-void va_end(va_list ap)
+void va_end( va_list ap )
 {
 
 }
@@ -55,7 +55,7 @@ void va_end(va_list ap)
 /**
  *
  */
-void va_copy(out va_list dest, va_list src)
+void va_copy( out va_list dest, va_list src )
 {
     dest = src;
 }
