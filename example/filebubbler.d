e9eaa3736979db9eb4f8a01ea66c18e2f0867c34
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2006-06-16 06:20:43 +0000

    another example; one that Tango actually uses

---
+++ example/filebubbler.d
@@ -0,0 +1,36 @@
+
+private import  tango.io.Console,
+                tango.io.FileScan,
+                tango.io.FileConst;
+
+/*******************************************************************************
+
+        This example sweeps a named sub-directory tree for html files,
+        and moves them to the current directory. The existing directory 
+        hierarchy is flattened into a naming sheme where a '.' is used
+        to replace the traditional path-separator
+
+        Used by the Tango project to help mange renderings of the source 
+        code.
+
+*******************************************************************************/
+
+void main(char[][] args)
+{       
+        void process (File file)
+        {
+                // get a mutated target filename
+                auto dst = file.getPath.replace (file.toString.dup, FileConst.PathSeparatorChar, '.');
+
+                // rename the file
+                file.rename (new FilePath (dst));
+        }
+
+
+        // sweep all html files in the specified subdir
+        if (args.length is 2)
+           (new FileScan).sweep(args[1], "html").files(&process);       
+        else
+           Cout ("usage is filebubbler subdir").newline;
+}
+
