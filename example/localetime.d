61b05a328bd1757a032734576c7a9db2daa71283
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2006-06-23 06:52:26 +0000

    example for the locale package

---
+++ example/localetime.d
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+        Example to format a locale-based time. For a default locale of 
+        en-gb, this examples formats in the following manner:
+
+        "Thu, 27 April 2006 18:20:47 +1"
+
+******************************************************************************/
+
+private import tango.io.Console;
+
+private import tango.text.locale.Core;
+
+void main ()
+{
+        Cout (DateTime.now.toString ("ddd, dd MMMM yyyy HH:mm:ss z")).newline;
+}
