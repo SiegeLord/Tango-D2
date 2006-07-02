8f7e872a20cf3dd13d266eca2bc8fe3f9aae4c8d
sean <sean@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2006-07-02 19:14:49 +0000

    Renamed build scripts to "build-X"

--- lib/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set TANGO_OLDHOME=%HOME%
-set HOME=%CD%
-make clean   -fwin32.mak
-make         -fwin32.mak
-make install -fwin32.mak
-make clean   -fwin32.mak
-set HOME=%TANGO_OLDHOME%
-pause
--- lib/gdcrt-build.sh
+++
@@ -1,5 +0,0 @@
-OLDHOME=$HOME
-export HOME=`pwd`
-./compiler/gdc/configure
-make
-export HOME=$OLDHOME
