c611977a6040b210327ad7abea0205584cff06cc
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2006-07-05 03:19:07 +0000

    minor cleanup

--- lib/compiler/dmd/genobj.d
+++ lib/compiler/dmd/genobj.d
@@ -195,16 +195,19 @@ class TypeInfo
     }
 
     // local function to compare two strings. Invoked by the default opCmp
-    private static int cmp(char[] s1, char[] s2)
+    private static int cmp (char[] s1, char[] s2)
     {
-        auto len = s1.length;
         int result;
+        auto len = s1.length;
 
         if (s2.length < len)
 	   len = s2.length;
+
         result = memcmp(s1, s2, len);
+
         if (result == 0)
 	    result = cast(int)s1.length - cast(int)s2.length;
+
         return result;
     }
 
@@ -216,7 +219,8 @@ class TypeInfo
         do {
            *--p = (val % 10) + '0';
            } while (val /= 10);
-        return p[0 .. tmp.length - (p - tmp.ptr)];
+
+        return tmp [(p - tmp.ptr) .. $];
     }
 }
 
