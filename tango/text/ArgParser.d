626e4a0b21bf2ce87b7173c4178447ae215d5e4b
larsivi <larsivi@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2006-07-16 18:07:29 +0000

    Imported latest revision from DDL

--- tango/text/ArgParser.d
+++ tango/text/ArgParser.d
@@ -1,27 +1,13 @@
 /*******************************************************************************
 
-        copyright:      Copyright (c) 2005 - 2006 Eric Anderton, 
-                        Lars Ivar Igesund. All rights reserved
+        copyright:      Copyright (c) 2005-2006 Lars Ivar Igesund, 
+                        Eric Anderton. All rights reserved
 
         license:        BSD style: $(LICENSE)
 
-        version:        Initial release: October 2005      
+        version:        Initial release: December 2005      
         
-        author:         Eric Anderton, Lars Ivar Igesund
-
-        This module defines a utility class that can parse your 
-        command line arguments.
-
-        A sample program would instantiate the ArgParser class, 
-        bind some delegates (can be anonymous), then call parse 
-        with the arguments as a parameter.
-
-        Note that the delegates must return the correct number of
-        consumed characters to ensure that the ArgParser operates 
-        correctly. Simple arguments that don't use the value parameter 
-        of the callback, should return 0 consumed characters. This 
-        behaviour will ensure that ArgParser will correctly handle 
-        all arguments, even when there are no space between them.
+        author:         Lars Ivar Igesund, Eric Anderton
 
 *******************************************************************************/
 
@@ -58,20 +44,21 @@ alias void delegate () ArgParserSimpleCallback;
     A utility class to parse and handle your command line arguments.
 */
 class ArgParser{
-        /**
-            A helper struct containing a callback and an id to, corresponding to
-            the argId passed to one of the bind methods.
-        */
-        protected struct PrefixCallback {
-            char[] id;
-            ArgParserCallback cb;
-        }       
+	/**
+	    A helper struct containing a callback and an id to, corresponding to
+	    the argId passed to one of the bind methods.
+	*/
+	protected struct PrefixCallback {
+	    char[] id;
+	    ArgParserCallback cb;
+	}	
 
     protected PrefixCallback[][char[]] bindings;
     protected DefaultArgParserCallback[char[]] defaultBindings;
     protected uint[char[]] prefixOrdinals;
     protected char[][] prefixSearchOrder;
     protected DefaultArgParserCallback defaultbinding;
+	private uint defaultOrdinal = 0;
 
     protected void addBinding(PrefixCallback pcb, char[] argPrefix){
         if (!(argPrefix in bindings)) {
@@ -118,7 +105,7 @@ class ArgParser{
         
         public uint adapterCallback(char[] value){
             callback();
-            return 0;
+            return value.length;
         }
     }
 
@@ -150,6 +137,9 @@ class ArgParser{
     public void bindDefault(char[] argPrefix, DefaultArgParserCallback callback){
         defaultBindings[argPrefix] = callback;
         prefixOrdinals[argPrefix] = 0;
+        if (!(argPrefix in bindings)) {
+            prefixSearchOrder ~= argPrefix;
+        }
     }
 
     /**
@@ -172,7 +162,6 @@ class ArgParser{
             arguments = the command line arguments from the application
     */
     public void parse(char[][] arguments){
-            uint defaultOrdinal = 0;
         if (bindings.length == 0) return;
 
         foreach (char[] arg; arguments) {
@@ -188,15 +177,17 @@ class ArgParser{
                     else {
                         argData = argData[prefix.length..$];
                     } 
-                    foreach (PrefixCallback cb; bindings[prefix]) {
-                        if (argData.length < cb.id.length) continue;
-                        uint cbil = cb.id.length;
-                        if (cb.id == argData[0..cbil]) {
-                            found = true;
-                            argData = argData[cbil..$];
-                            uint consumed = cb.cb(argData);
-                            argData = argData[consumed..$];
-                            break;
+                    if (prefix in bindings) {
+                        foreach (PrefixCallback cb; bindings[prefix]) {
+                            if (argData.length < cb.id.length) continue;
+                            uint cbil = cb.id.length;
+                            if (cb.id == argData[0..cbil]) {
+                                found = true;
+                                argData = argData[cbil..$];
+                                uint consumed = cb.cb(argData);
+                                argData = argData[consumed..$];
+                                break;
+                            }
                         }
                     }
                     if (found) {
@@ -206,6 +197,7 @@ class ArgParser{
                         uint consumed = defaultBindings[prefix](argData,prefixOrdinals[prefix]);
                         argData = argData[consumed..$];
                         prefixOrdinals[prefix]++;
+                        found = true;
                         break;
                     }
                     argData = argOrig;
@@ -224,4 +216,81 @@ class ArgParser{
             }
         }
     }
+}
+
+unittest {
+
+    //import mango.text.String;
+
+    ArgParser parser = new ArgParser();
+    bool h = h2 = b = bb = false;
+    bool boolean = false;
+    int n;
+
+    parser.bind("--", "h2", delegate void(){
+        h2 = true;
+    });
+
+    parser.bind("-", "h", delegate void(){
+        h = true;
+    });
+
+    parser.bind("-", "bb", delegate void(){
+        bb = true;
+    });
+
+    parser.bind("-", "b", delegate void(){
+        b = true;
+    });
+
+    parser.bind("-", "n", delegate uint(char[] value){
+        assert(value.length > 2);
+        assert(value[1] == '=');
+        n = toInt(value[2..5]);
+        return 5;
+    });
+
+    parser.bind("-", "bool", delegate uint(char[] value){
+        assert(value.length == 9);
+        assert(value[4] == '=');
+        if (value[5..9] == "true") {
+            boolean = true;
+        }
+        else {
+            assert(false);
+        }
+        return 9;
+    });
+
+    parser.bindDefault(delegate uint(char[] value, uint ordinal){
+        assert (ordinal < 2);
+        ordinalCount = ordinal;
+        if (ordinal == 0) {
+            assert(value == "ordinalTest1");
+        }
+        else if (ordinal == 1) {
+            assert(value == "ordinalTest2");
+        }
+        return value.length;
+    });
+
+    parser.bindDefault("-", delegate uint(char[] value, uint ordinal){
+        assert(ordinal < 2);
+        dashOrdinalCount = ordinal;
+        if (ordinal == 0) {
+            assert(value == "dashTest1");
+        }
+        else if (ordinal == 1) {
+            assert(value == "dashTest1");
+        }
+        return value.length;
+    });
+
+    parser.bindDefault("@", delegate uint(char[] value, uint ordinal){
+        assert (value == "atTest");
+        return value.length;
+    });
+
+    
+
 }
