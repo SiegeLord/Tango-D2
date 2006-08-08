653d4891c5ce63c108d1ccc208703a7742351d2f
sean <sean@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2006-08-08 01:15:45 +0000

    * Added some missing template parameters. * Reformatted module to create better visual separation between routines. * Added new functions: findIf, rfindIf, lbound, ubound.

--- tango/core/Array.d
+++ tango/core/Array.d
@@ -12,6 +12,11 @@ module tango.core.Array;
 private import tango.core.Traits;
 
 
+////////////////////////////////////////////////////////////////////////////////
+// Find
+////////////////////////////////////////////////////////////////////////////////
+
+
 template find_( Elem, Pred )
 {
     static assert( isCallableType!(Pred) );
@@ -144,6 +149,158 @@ debug( UnitTest )
 }
 
 
+////////////////////////////////////////////////////////////////////////////////
+// Reverse Find
+////////////////////////////////////////////////////////////////////////////////
+
+
+template rfind_( Elem, Pred )
+{
+    static assert( isCallableType!(Pred) );
+
+
+    size_t fn( Elem[] buf, Elem pat )
+    {
+        if( buf.length == 0 )
+            return size_t.max;
+
+        size_t pos = buf.length;
+
+        do
+        {
+            if( buf[--pos] == pat )
+                return pos;
+        } while( pos > 0 );
+        return size_t.max;
+    }
+
+
+    size_t fn( Elem[] buf, Elem pat, Pred pred )
+    {
+        if( buf.length == 0 )
+            return size_t.max;
+
+        size_t pos = buf.length;
+
+        do
+        {
+            if( pred( buf[--pos], pat ) )
+                return pos;
+        } while( pos > 0 );
+        return size_t.max;
+    }
+
+
+    size_t fn( Elem[] buf, Elem[] pat )
+    {
+        if( buf.length == 0 ||
+            pat.length == 0 ||
+            buf.length < pat.length )
+        {
+            return size_t.max;
+        }
+
+        size_t pos = buf.length - pat.length + 1;
+
+        do
+        {
+            if( buf[--pos] == pat[0] )
+            {
+                size_t mat = 0;
+
+                do
+                {
+                    if( ++mat >= pat.length )
+                        return pos - pat.length + 1;
+                    if( ++pos >= buf.length )
+                        return size_t.max;
+                } while( buf[pos] == pat[mat] );
+                pos -= mat;
+            }
+        } while( pos > 0 );
+        return size_t.max;
+    }
+
+
+    size_t fn( Elem[] buf, Elem[] pat, Pred pred )
+    {
+        if( buf.length == 0 ||
+            pat.length == 0 ||
+            buf.length < pat.length )
+        {
+            return size_t.max;
+        }
+
+        size_t pos = buf.length - pat.length + 1;
+
+        do
+        {
+            if( pred( buf[--pos], pat[0] ) )
+            {
+                size_t mat = 0;
+
+                do
+                {
+                    if( ++mat >= pat.length )
+                        return pos - pat.length + 1;
+                    if( ++pos >= buf.length )
+                        return size_t.max;
+                } while( pred( buf[pos], pat[mat] ) );
+                pos -= mat;
+            }
+        } while( pos > 0 );
+        return size_t.max;
+    }
+}
+
+
+template rfind( Elem, Pred = bool function( Elem, Elem ) )
+{
+    alias rfind_!(Elem, Pred).fn rfind;
+}
+
+
+debug( UnitTest )
+{
+  unittest
+  {
+    // rfind element
+    assert( rfind!(char)( "", 'a' ) == size_t.max );
+    assert( rfind!(char)( "abc", 'a' ) == 0 );
+    assert( rfind!(char)( "abc", 'b' ) == 1 );
+    assert( rfind!(char)( "abc", 'c' ) == 2 );
+    assert( rfind!(char)( "abc", 'd' ) == size_t.max );
+
+    // null parameters
+    assert( rfind!(char)( "", "" ) == size_t.max );
+    assert( rfind!(char)( "a", "" ) == size_t.max );
+    assert( rfind!(char)( "", "a" ) == size_t.max );
+
+    // exact match
+    assert( rfind!(char)( "abc", "abc" ) == 0 );
+
+    // simple substring match
+    assert( rfind!(char)( "abc", "a" ) == 0 );
+    assert( rfind!(char)( "abca", "a" ) == 3 );
+    assert( rfind!(char)( "abc", "b" ) == 1 );
+    assert( rfind!(char)( "abc", "c" ) == 2 );
+    assert( rfind!(char)( "abc", "d" ) == size_t.max );
+
+    // multi-char substring match
+    assert( rfind!(char)( "abc", "ab" ) == 0 );
+    assert( rfind!(char)( "abcab", "ab" ) == 3 );
+    assert( rfind!(char)( "abc", "bc" ) == 1 );
+    assert( rfind!(char)( "abc", "ac" ) == size_t.max );
+    assert( rfind!(char)( "abracadabrabra", "abracadabra" ) == 0 );
+  }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// KMP Find
+////////////////////////////////////////////////////////////////////////////////
+
+
 template kfind_( Elem, Pred )
 {
     static assert( isCallableType!(Pred) );
@@ -270,7 +427,7 @@ template kfind_( Elem, Pred )
 
 template kfind( Elem, Pred = bool function( Elem, Elem ) )
 {
-    alias kfind_!(Elem).fn kfind;
+    alias kfind_!(Elem, Pred).fn kfind;
 }
 
 
@@ -310,146 +467,9 @@ debug( UnitTest )
 }
 
 
-template rfind_( Elem, Pred )
-{
-    static assert( isCallableType!(Pred) );
-
-
-    size_t fn( Elem[] buf, Elem pat )
-    {
-        if( buf.length == 0 )
-            return size_t.max;
-
-        size_t pos = buf.length;
-
-        do
-        {
-            if( buf[--pos] == pat )
-                return pos;
-        } while( pos > 0 );
-        return size_t.max;
-    }
-
-
-    size_t fn( Elem[] buf, Elem pat, Pred pred )
-    {
-        if( buf.length == 0 )
-            return size_t.max;
-
-        size_t pos = buf.length;
-
-        do
-        {
-            if( pred( buf[--pos], pat ) )
-                return pos;
-        } while( pos > 0 );
-        return size_t.max;
-    }
-
-
-    size_t fn( Elem[] buf, Elem[] pat )
-    {
-        if( buf.length == 0 ||
-            pat.length == 0 ||
-            buf.length < pat.length )
-        {
-            return size_t.max;
-        }
-
-        size_t pos = buf.length - pat.length + 1;
-
-        do
-        {
-            if( buf[--pos] == pat[0] )
-            {
-                size_t mat = 0;
-
-                do
-                {
-                    if( ++mat >= pat.length )
-                        return pos - pat.length + 1;
-                    if( ++pos >= buf.length )
-                        return size_t.max;
-                } while( buf[pos] == pat[mat] );
-                pos -= mat;
-            }
-        } while( pos > 0 );
-        return size_t.max;
-    }
-
-
-    size_t fn( Elem[] buf, Elem[] pat, Pred pred )
-    {
-        if( buf.length == 0 ||
-            pat.length == 0 ||
-            buf.length < pat.length )
-        {
-            return size_t.max;
-        }
-
-        size_t pos = buf.length - pat.length + 1;
-
-        do
-        {
-            if( pred( buf[--pos], pat[0] ) )
-            {
-                size_t mat = 0;
-
-                do
-                {
-                    if( ++mat >= pat.length )
-                        return pos - pat.length + 1;
-                    if( ++pos >= buf.length )
-                        return size_t.max;
-                } while( pred( buf[pos], pat[mat] ) );
-                pos -= mat;
-            }
-        } while( pos > 0 );
-        return size_t.max;
-    }
-}
-
-
-template rfind( Elem, Pred = bool function( Elem, Elem ) )
-{
-    alias rfind_!(Elem).fn rfind;
-}
-
-
-debug( UnitTest )
-{
-  unittest
-  {
-    // rfind element
-    assert( rfind!(char)( "", 'a' ) == size_t.max );
-    assert( rfind!(char)( "abc", 'a' ) == 0 );
-    assert( rfind!(char)( "abc", 'b' ) == 1 );
-    assert( rfind!(char)( "abc", 'c' ) == 2 );
-    assert( rfind!(char)( "abc", 'd' ) == size_t.max );
-
-    // null parameters
-    assert( rfind!(char)( "", "" ) == size_t.max );
-    assert( rfind!(char)( "a", "" ) == size_t.max );
-    assert( rfind!(char)( "", "a" ) == size_t.max );
-
-    // exact match
-    assert( rfind!(char)( "abc", "abc" ) == 0 );
-
-    // simple substring match
-    assert( rfind!(char)( "abc", "a" ) == 0 );
-    assert( rfind!(char)( "abca", "a" ) == 3 );
-    assert( rfind!(char)( "abc", "b" ) == 1 );
-    assert( rfind!(char)( "abc", "c" ) == 2 );
-    assert( rfind!(char)( "abc", "d" ) == size_t.max );
-
-    // multi-char substring match
-    assert( rfind!(char)( "abc", "ab" ) == 0 );
-    assert( rfind!(char)( "abcab", "ab" ) == 3 );
-    assert( rfind!(char)( "abc", "bc" ) == 1 );
-    assert( rfind!(char)( "abc", "ac" ) == size_t.max );
-    assert( rfind!(char)( "abracadabrabra", "abracadabra" ) == 0 );
-  }
-}
+////////////////////////////////////////////////////////////////////////////////
+// KMP Reverse Find
+////////////////////////////////////////////////////////////////////////////////
 
 
 template krfind_( Elem, Pred )
@@ -594,7 +614,7 @@ template krfind_( Elem, Pred )
 
 template krfind( Elem, Pred = bool function( Elem, Elem ) )
 {
-    alias krfind_!(Elem).fn krfind;
+    alias krfind_!(Elem, Pred).fn krfind;
 }
 
 
@@ -630,5 +650,230 @@ debug( UnitTest )
     assert( krfind!(char)( "abc", "bc" ) == 1 );
     assert( krfind!(char)( "abc", "ac" ) == size_t.max );
     assert( krfind!(char)( "abracadabrabra", "abracadabra" ) == 0 );
+  }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Find-If
+////////////////////////////////////////////////////////////////////////////////
+
+
+template findIf_( Elem, Pred )
+{
+    static assert( isCallableType!(Pred) );
+
+
+    size_t fn( Elem[] buf, Pred pred )
+    {
+        foreach( size_t pos, Elem cur; buf )
+        {
+            if( pred( cur ) )
+                return pos;
+        }
+        return size_t.max;
+    }
+}
+
+
+template findIf( Elem, Pred = bool function( Elem ) )
+{
+    alias findIf_!(Elem, Pred).fn findIf;
+}
+
+
+debug( UnitTest )
+{
+  unittest
+  {
+
+  }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Reverse Find-If
+////////////////////////////////////////////////////////////////////////////////
+
+
+template rfindIf_( Elem, Pred )
+{
+    static assert( isCallableType!(Pred) );
+
+
+    size_t fn( Elem[] buf, Pred pred )
+    {
+        if( buf.length == 0 )
+            return size_t.max;
+
+        size_t pos = buf.length;
+
+        do
+        {
+            if( pred( buf[--pos] ) )
+                return pos;
+        } while( pos > 0 );
+        return size_t.max;
+    }
+}
+
+
+template rfindIf( Elem, Pred = bool function( Elem ) )
+{
+    alias rfindIf_!(Elem, Pred).fn rfindIf;
+}
+
+
+debug( UnitTest )
+{
+  unittest
+  {
+
+  }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Lower Bound
+////////////////////////////////////////////////////////////////////////////////
+
+
+template lbound_( Elem, Pred )
+{
+    static assert( isCallableType!(Pred) );
+
+
+    size_t fn( Elem[] buf, Elem pat )
+    {
+        size_t  beg   = 0,
+                end   = buf.length,
+                mid   = end / 2;
+
+        while( beg + 1 < end )
+        {
+            if( buf[mid] < pat )
+                beg = mid + 1;
+            else
+                end = mid;
+            mid = beg + ( end - beg ) / 2;
+        }
+        return mid;
+    }
+
+
+    size_t fn( Elem[] buf, Elem pat, Pred pred )
+    {
+        size_t  beg   = 0,
+                end   = buf.length,
+                mid   = end / 2;
+
+        while( beg + 1 < end )
+        {
+            if( pred( buf[mid], pat ) )
+                beg = mid + 1;
+            else
+                end = mid;
+            mid = beg + ( end - beg ) / 2;
+        }
+        return mid;
+    }
+}
+
+
+template lbound( Elem, Pred = bool function( Elem, Elem ) )
+{
+    alias lbound_!(Elem, Pred).fn lbound;
+}
+
+
+debug( UnitTest )
+{
+  unittest
+  {
+    int[5] buf;
+
+    buf[0] = 1;
+    buf[1] = 2;
+    buf[2] = 4;
+    buf[3] = 5;
+    buf[4] = 6;
+
+    assert( lbound!(int)( buf, 0 ) == 0 );
+    assert( lbound!(int)( buf, 7 ) == 5 );
+    assert( lbound!(int)( buf, 3 ) == 2 );
+    assert( lbound!(int)( buf, 4 ) == 2 );
+  }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Upper Bound
+////////////////////////////////////////////////////////////////////////////////
+
+
+template ubound_( Elem, Pred )
+{
+    static assert( isCallableType!(Pred) );
+
+
+    size_t fn( Elem[] buf, Elem pat )
+    {
+        size_t  beg   = 0,
+                end   = buf.length,
+                mid   = end / 2;
+
+        while( beg + 1 < end )
+        {
+            if( !( pat < buf[mid] ) )
+                beg = mid + 1;
+            else
+                end = mid;
+            mid = beg + ( end - beg ) / 2;
+        }
+        return mid;
+    }
+
+
+    size_t fn( Elem[] buf, Elem pat, Pred pred )
+    {
+        size_t  beg   = 0,
+                end   = buf.length,
+                mid   = end / 2;
+
+        while( beg + 1 < end )
+        {
+            if( !pred( pat, buf[mid] ) )
+                beg = mid + 1;
+            else
+                end = mid;
+            mid = beg + ( end - beg ) / 2;
+        }
+        return mid;
+    }
+}
+
+
+template ubound( Elem, Pred = bool function( Elem, Elem ) )
+{
+    alias ubound_!(Elem, Pred).fn ubound;
+}
+
+
+debug( UnitTest )
+{
+  unittest
+  {
+    int[5] buf;
+
+    buf[0] = 1;
+    buf[1] = 2;
+    buf[2] = 4;
+    buf[3] = 5;
+    buf[4] = 6;
+
+    assert( ubound!(int)( buf, 0 ) == 0 );
+    assert( ubound!(int)( buf, 7 ) == 5 );
+    assert( ubound!(int)( buf, 3 ) == 2 );
+    assert( ubound!(int)( buf, 4 ) == 3 );
   }
 }
