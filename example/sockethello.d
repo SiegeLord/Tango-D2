8f4294fbb73e2e239f1863ce36ec2da103e75418
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2006-08-22 19:06:35 +0000

    new example

---
+++ example/sockethello.d
@@ -0,0 +1,37 @@
+/*******************************************************************************
+
+        Shows how to create a basic socket client, and how to converse with
+        a remote server. 
+
+*******************************************************************************/
+
+private import  tango.io.Console;
+
+private import  tango.net.SocketConduit, 
+                tango.net.InternetAddress;
+
+version (Win32)
+         pragma (lib, "wsock32");
+
+
+/*******************************************************************************
+
+*******************************************************************************/
+
+void main()
+{
+        // make a connection request to the server
+        auto request = new SocketConduit;
+        request.connect (new InternetAddress ("localhost", 8080));
+        request.write ("hello\n");
+
+        // wait for response (there is an optional timeout supported)
+        char[64] response;
+        request.read (response);
+
+        // close socket
+        request.close;
+
+        // display server response
+        Cout (response).newline;
+}
