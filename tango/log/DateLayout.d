db83e786c4a1dda999828c6407cbc8bdff7fc77b
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2006-08-23 04:05:04 +0000

    Updated formatting to C# style

--- tango/log/DateLayout.d
+++ tango/log/DateLayout.d
@@ -44,9 +44,22 @@ public class DateLayout : Layout
                 // point formatter at the output buffer
                 SprintStruct sprint;
                 sprint.ctor (event.scratch.content);
-        
+/+        
                 // format fields according to ISO-8601
                 return sprint ("%04d-%02d-%02d %02d:%02d:%02d,%03d %-6s %s - ",
+                               fields.year, 
+                               fields.month, 
+                               fields.day, 
+                               fields.hour, 
+                               fields.min, 
+                               fields.sec, 
+                               event.getTime % 1000,
+                               event.getLevelName,
+                               event.getName
+                               );            
++/
+                // format fields according to ISO-8601
+                return sprint ("{0:d4}-{1:d2}-{2:d2} {3:d2}:{4:d2}:{5:d2},{6:d3} {7,-6} {8} - ",
                                fields.year, 
                                fields.month, 
                                fields.day, 
