1467efa3369c567b4f35827797bc6bd37e8d8f81
larsivi <larsivi@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2006-10-30 16:39:22 +0000

    Added two new examples showing a couple of simple formatting usecases.

---
+++ example/formatalign.d
@@ -0,0 +1,18 @@
+/**
+
+  Example showing how the alignment component in a format string argument works.
+
+*/
+
+import tango.io.Stdout;
+
+void main(){
+    char[] myFName = "Johnny";
+    Stdout.formatln("First Name = |{0,15}|", myFName);
+    Stdout.formatln("Last Name = |{0,15}|", "Foo de Bar");
+
+    Stdout.formatln("First Name = |{0,-15}|", myFName);
+    Stdout.formatln("Last Name = |{0,-15}|", "Foo de Bar");
+
+    Stdout.formatln("First name = |{0,5}|", myFName);
+}
