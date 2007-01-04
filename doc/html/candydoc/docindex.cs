185b18dc7258810b66d097df0777eeeae51e6768
brad <brad@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-01-04 22:21:38 +0000

    API index page for auto doc gen

---
+++ doc/html/candydoc/docindex.cs
@@ -0,0 +1,21 @@
+<html>
+ <head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <link rel="stylesheet" href="css/style.css" type="text/css" />
+  <title>Tango API Index</title>
+ </head>
+ <body>
+  <a href="../../">Back to Project Page</a>
+  <h2>Tango API Index</h2>
+  <p>Use the Browser incremental search facility to quickly locate a particular
+    module name.</p>
+  <p>Source code for each module is available by clicking the <a href="#">big
+    blue title</a> at the top of each page, and all other links therein lead to
+    Wiki annotations. Packages are sorted by name:</p>
+  <ul>
+<?cs each:module = modules ?>   <li><a href="<?cs var:module ?>.html"><?cs 
+  var:module ?></a></li>
+<?cs /each ?>
+  </ul>
+ </body>
+</html>
