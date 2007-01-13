729f1ebefc2d4ce9b4bab2c6356b7f78cec6d29c
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-01-13 22:00:16 +0000

    now taking advantage of the featherweight formatter

--- tango/util/log/DateLayout.d
+++ tango/util/log/DateLayout.d
@@ -15,18 +15,22 @@ module tango.util.log.DateLayout;
 private import  tango.util.log.Event,
                 tango.util.log.Layout;
 
+private import  tango.text.Util;
+
 private import  tango.core.Epoch;
 
-private import  tango.text.convert.Format;
+private import  tango.text.convert.Integer;
 
 /*******************************************************************************
 
-        A layout with ISO-8601 date information prefixed to each message.
+        A layout with ISO-8601 date information prefixed to each message
        
 *******************************************************************************/
 
 public class DateLayout : Layout
 {
+        private static char[6] spaces = ' ';
+
         /***********************************************************************
                 
                 Format message attributes into an output buffer and return
@@ -37,21 +41,35 @@ public class DateLayout : Layout
         char[] header (Event event)
         {
                 Epoch.Fields fields;
-
+                char[20] tmp = void;
+                char[] level = event.getLevelName;
+                
                 // convert time to field values
                 fields.asUtcTime (event.getEpochTime);
                                 
-                // format fields according to ISO-8601
-                return Formatter.sprint (event.scratch.content, "{0:d4}-{1:d2}-{2:d2} {3:d2}:{4:d2}:{5:d2},{6:d3} {7,-6} {8} - ",
-                                         fields.year, 
-                                         fields.month, 
-                                         fields.day, 
-                                         fields.hour, 
-                                         fields.min, 
-                                         fields.sec, 
-                                         event.getTime % 1000,
-                                         event.getLevelName,
-                                         event.getName
-                                        );            
+                // format fields according to ISO-8601 (lightweight formatter)
+                return layout (event.scratch.content, "%0-%1-%2 %3:%4:%5,%6 %7%8 %9 - ", 
+                               convert (tmp[0..4],   fields.year),
+                               convert (tmp[4..6],   fields.month),
+                               convert (tmp[6..8],   fields.day),
+                               convert (tmp[8..10],  fields.hour),
+                               convert (tmp[10..12], fields.min),
+                               convert (tmp[12..14], fields.sec),
+                               convert (tmp[14..17], fields.ms),
+                               spaces [0 .. $-level.length],
+                               level,
+                               event.getName
+                              );
+        }
+        
+        /**********************************************************************
+
+                Convert an integer to a zero prefixed text representation
+
+        **********************************************************************/
+
+        private char[] convert (char[] tmp, int i)
+        {
+                return format (tmp, cast(long) i, Format.Unsigned, Flags.Zero);
         }
 }
