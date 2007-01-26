aedf78004815bd51842ad2ec52bc56ec99a7203b
larsivi <larsivi@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-01-26 10:53:33 +0000

    Added some search statistics - second run found and printed 7192 matches in 806295 files in seven seconds

--- example/conduits/filescanregex.d
+++ example/conduits/filescanregex.d
@@ -13,6 +13,9 @@ import tango.io.FileScan,
 
 int main(char[][] args) {
 
+    ulong checked = 0;
+    ulong found = 0;
+    
     if (args.length < 2) {
         Stdout("Please pass a directory to search").newline; 
         return 0;
@@ -22,11 +25,18 @@ int main(char[][] args) {
     scope regex =  Regex(r"\.(d|obj)$");
 
     scan(new FilePath(args[1]), delegate bool (FileProxy fp, bool isDir) { 
+            checked++;
             return isDir || regex.test(fp.toUtf8); 
     });
     
-    foreach (File f; scan.files)
+
+    foreach (File f; scan.files) {
+        found++;
         Stdout(f.toUtf8).newline;
+    }
+
+    Stdout.format("Checked {0} files", checked).newline;
+    Stdout.format("Found {0} matches", found).newline;
 
     return 0;
 }
