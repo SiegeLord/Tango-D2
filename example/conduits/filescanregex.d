6f6ae5c6bb0194c9a4e4bcb38f39333b43361eca
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-01-26 20:59:38 +0000

    small fixes

--- example/conduits/filescanregex.d
+++ example/conduits/filescanregex.d
@@ -6,37 +6,27 @@
 
 **************************************************************/
 
-import tango.io.FileScan,
-       tango.io.File,
-       tango.text.Regex,
-       tango.io.Stdout;
+import tango.io.File,
+       tango.io.Stdout,
+       tango.io.FileScan,
+       tango.text.Regex;
 
-int main(char[][] args) {
-
-    ulong checked = 0;
-    ulong found = 0;
-    
+void main(char[][] args) {
     if (args.length < 2) {
         Stdout("Please pass a directory to search").newline; 
-        return 0;
+        return;
     }
 
     scope scan = new FileScan;
     scope regex =  Regex(r"\.(d|obj)$");
 
     scan(new FilePath(args[1]), delegate bool (FileProxy fp, bool isDir) { 
-            checked++;
-            return isDir || regex.test(fp.toUtf8); 
+         return isDir || regex.test(fp.toUtf8); 
     });
     
 
-    foreach (File f; scan.files) {
-        found++;
-        Stdout(f.toUtf8).newline;
-    }
+    foreach (File file; scan.files)
+             Stdout(file).newline;
 
-    Stdout.format("Checked {0} files", checked).newline;
-    Stdout.format("Found {0} matches", found).newline;
-
-    return 0;
+    Stdout.formatln("Found {0} matches in {1} entries", scan.files.length, scan.inspected);
 }
