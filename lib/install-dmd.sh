097041b419a78a09892c63facf358a82b92a5c0b
Jim Panic <Jim Panic@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-02-07 19:44:36 +0000

    Hopefully fixed the last few bugs in the installation script..

--- lib/install-dmd.sh
+++ lib/install-dmd.sh
@@ -41,7 +41,7 @@ DMD_INSTALL=0
 DSSS_INSTALL=0
 # 42.
 #
-CLEANALL=0
+CLEANALL=1
 #DEBUG=0
 #
 #SIMULATE=0
@@ -51,8 +51,11 @@ CLEANALL=0
 
 # 'exception', prints first parameter and exits script totally.
 die() {
-	echo "$1"
-	exit 0
+	CLEANALL=0
+	RET=$1
+	shift
+	echo "$@"
+	exit $1
 }
 
 # prints usage
@@ -117,29 +120,26 @@ EOF
 
 download_dmd() {
 	echo "Downloading dmd.zip from ${DMD_MIRROR}..."
-	wget -c ${DMD_MIRROR}/${DMD_FILENAME} || die "Error downloading DMD. Aborting."
+	wget -c ${DMD_MIRROR}/${DMD_FILENAME} || die 1 "Error downloading DMD. Aborting."
 }
 
 download_tango() {
 	echo "Downloading Tango from ${TANGO_MIRROR} (subversion required)..."
 
 	echo "..creating temporary directory tango/..."
-	mkdir -p tango || die "Error while creating temporary directory."
+	mkdir -p tango || die 1 "Error while creating temporary directory."
 
 	echo "..changing directory to tango/..."
-#	if [ ${SIMULATE} = 0 ]
-#	then
-		cd tango || die "Error while changing directory (tango/)."
-#	fi
+	cd tango || die 1 "Error while changing directory (tango/)."
 
 	echo "..checking out tango/trunk (quietly). This may take some time..."
-	svn checkout -q ${TANGO_REPOSITORY} || die "Error while checking out."
+	svn checkout -q ${TANGO_REPOSITORY} || die 1 "Error while checking out."
 
 	echo "..changing directory to tango/trunk/..."
-	cd trunk || die "Error while changing directory (tango/trunk/)."
+	cd trunk || die 1 "Error while changing directory (tango/trunk/)."
 
 	echo "..moving dmd archive to current directory..."
-	mv ../../${DMD_FILENAME} . || die "Error moving dmd archive to tango/trunk/."
+	mv ../../${DMD_FILENAME} . || die 1 "Error moving dmd archive to tango/trunk/."
 }
 
 install_dmd() {
@@ -148,74 +148,70 @@ install_dmd() {
 	copy_dmd_include
 	copy_dmd_lib
 	copy_dmd_doc
-
-	# seems like everything worked fine so far.. lets clean all.
-	CLEANALL=1
-
 	cleanup_dmd
 }
 
 unzip_dmd() {
 	if [ ! -f ${DMD_FILENAME} ]
 	then
-		die "Could not find dmd archive. Please use --download-dmd or manually download the file from ${DMD_MIRROR}. Aborting."
+		die 1 "Could not find dmd archive. Please use --download-dmd or manually download the file from ${DMD_MIRROR}. Aborting."
 	fi
 
 	echo "Extracting zip file..."
-	unzip -q -d . ${DMD_FILENAME} || die 'Could not extract zip file. Aborting.'
+	unzip -q -d . ${DMD_FILENAME} || die 1 'Could not extract zip file. Aborting.'
 }
 
 copy_dmd() {
 	echo "Copying compiler binaries..."
 
 	echo "..creating ${PREFIX}/bin"
-	mkdir -p ${PREFIX}/bin || die "Error creating directory (${PREFIX}/bin)."
+	mkdir -p ${PREFIX}/bin || die 1 "Error creating directory (${PREFIX}/bin)."
 
 	echo "..settin executable flag for dmd, objasm, dumpobj and rdmd..."
-	chmod +x dmd/bin/{dmd,obj2asm,dumpobj,rdmd} || die "Error chmod-ing (dmd, objasm, dumpobj, rdmd)."
+	chmod +x dmd/bin/{dmd,obj2asm,dumpobj,rdmd} || die 1 "Error chmod-ing (dmd, objasm, dumpobj, rdmd)."
 
 	echo "..copying executable files to ${PREFIX}/bin..."
-	cp dmd/bin/dmd ${PREFIX}/bin || die "Error copying dmd/bin/dmd."
-	cp dmd/bin/obj2asm ${PREFIX}/bin || die "Error copying dmd/bin/objasm."
-	cp dmd/bin/dumpobj ${PREFIX}/bin || die "Error copying dmd/bin/dumpobj."
-	cp dmd/bin/rdmd ${PREFIX}/bin || die "Error copying dmd/bin/rdmd."
+	cp dmd/bin/dmd ${PREFIX}/bin || die 1 "Error copying dmd/bin/dmd."
+	cp dmd/bin/obj2asm ${PREFIX}/bin || die 1 "Error copying dmd/bin/objasm."
+	cp dmd/bin/dumpobj ${PREFIX}/bin || die 1 "Error copying dmd/bin/dumpobj."
+	cp dmd/bin/rdmd ${PREFIX}/bin || die 1 "Error copying dmd/bin/rdmd."
 }
 
 copy_dmd_include() {
 	echo "Copying runtime library sources..."
 
 	echo "..creating ${PREFIX}/include"
-	mkdir -p ${PREFIX}/include || die "Error creating ${PREFIX}/include."
+	mkdir -p ${PREFIX}/include || die 1 "Error creating ${PREFIX}/include."
 
 	echo "..copying files"
-	cp -r dmd/src/phobos ${PREFIX}/include || die "Error copying library sources to ${PREFIX}/include."
+	cp -r dmd/src/phobos ${PREFIX}/include || die 1 "Error copying library sources to ${PREFIX}/include."
 }
 
 copy_dmd_doc() {
 	echo "Copying D documentation and samples (Phobos)..."
 
 	echo "..creating directories ${PREFIX}/doc/dmd/{src,samples}..."
-	mkdir -p {${PREFIX}/doc/dmd/samples,${PREFIX}/doc/dmd/src} || die "Error creating documentation directories."
+	mkdir -p {${PREFIX}/doc/dmd/samples,${PREFIX}/doc/dmd/src} || die 1 "Error creating documentation directories."
 
-	cp -r dmd/html/d/* ${PREFIX}/doc/dmd/ || die "Error copying documentation."
-	cp -r dmd/samples/d/* ${PREFIX}/doc/dmd/samples/ || die "Error copying samples."
-	cp -r dmd/src/dmd/* ${PREFIX}/doc/dmd/src/ || die "Error copying source."
+	cp -r dmd/html/d/* ${PREFIX}/doc/dmd/ || die 1 "Error copying documentation."
+	cp -r dmd/samples/d/* ${PREFIX}/doc/dmd/samples/ || die 1 "Error copying samples."
+	cp -r dmd/src/dmd/* ${PREFIX}/doc/dmd/src/ || die 1 "Error copying source."
 }
 
 copy_dmd_lib() {
 	echo "Copying runtime library..."
 
 	echo "..creating ${PREFIX}/lib..."
-	mkdir -p ${PREFIX}/lib || die "Error creating directory for runtime library (${PREFIX}/lib)."
+	mkdir -p ${PREFIX}/lib || die 1 "Error creating directory for runtime library (${PREFIX}/lib)."
 
 	echo "..copying original libphobos.a"
-	cp dmd/lib/libphobos.a ${PREFIX}/lib/ || die "Error copying libphobos.a."
-#	cp dmd/lib/phobos.lib ${PREFIX}/lib/ || die "Error copying phobos.lib."
+	cp dmd/lib/libphobos.a ${PREFIX}/lib/ || die 1 "Error copying libphobos.a."
+#	cp dmd/lib/phobos.lib ${PREFIX}/lib/ || die 1 "Error copying phobos.lib."
 }
 
 copy_dmd_man() {
 	echo "Copying manpages to ${MAN_DIR}"
-	cp dmd/man/man1/* ${MAN_DIR} || die "Error copying manpages."
+	cp dmd/man/man1/* ${MAN_DIR} || die 1 "Error copying manpages."
 }
 
 install_tango() {
@@ -229,18 +225,18 @@ install_tango() {
 		echo "...checking if directory already exists (svn export will fail, if so)..."
 		if [ -f ${PREFIX}/include/tango ]
 		then
-			die "Error, destination does already exist; please delete it or change prefix."
+			die 1 "Error, destination does already exist; please delete it or change prefix."
 		else
 			echo "....everything fine; doing svn export..."
 			mkdir -p ${PREFIX}/include
-			svn export --force . ${PREFIX}/include/tango || die "Error exporting Tango via svn export."
+			svn export --force . ${PREFIX}/include/tango || die 1 "Error exporting Tango via svn export."
 		fi
 	else
 		echo "...no subversion files found, using normal copy method..."
 		echo "...creating directory ${PREFIX}/include..."
 
-		mkdir -p ${PREFIX}/include/tango || die "Error creating Tango directory."
-		cp -r ./* ${PREFIX}/include/tango || die "Error copying Tango to new directory."
+		mkdir -p ${PREFIX}/include/tango || die 1 "Error creating Tango directory."
+		cp -r ./* ${PREFIX}/include/tango || die 1 "Error copying Tango to new directory."
 	fi
 
 	if [ ! -f "lib/libphobos.a" ]
@@ -249,7 +245,7 @@ install_tango() {
 		cd lib/
 
 		echo "...building base runtime library"
-		./build-dmd.sh || die "Error building library."
+		./build-dmd.sh || die 1 "Error building library."
 
 		echo "...changing directory: ../"
 		cd ../
@@ -258,14 +254,14 @@ install_tango() {
 	if [ -f "${PREFIX}/lib/libphobos.a" ]
 	then
 		echo "...making a backup of the original phobos library"
-		mv ${PREFIX}/lib/libphobos.a ${PREFIX}/lib/original_libphobos.a || die "Error renaming original libphobos.a"
+		mv ${PREFIX}/lib/libphobos.a ${PREFIX}/lib/original_libphobos.a || die 1 "Error renaming original libphobos.a"
 	fi
 
 	if [ -f "lib/libphobos.a" ]
 	then
-		cp lib/libphobos.a ${PREFIX}/lib/libphobos.a || die "Error copying Tango's libphobos.a replacement to ${PREFIX}/lib/libphobos.a"
+		cp lib/libphobos.a ${PREFIX}/lib/libphobos.a || die 1 "Error copying Tango's libphobos.a replacement to ${PREFIX}/lib/libphobos.a"
 	else
-		echo "Library has not been built, yet the build script itself did not throw an error. Please report this to a developer."
+		die 1 "Library has not been built, yet the build script itself did not throw an error. Please report this to a developer."
 	fi
 }
 
@@ -278,33 +274,46 @@ dmd_conf_install() {
 		echo "..no dmd.conf found, installing default one to /etc/dmd.conf..."
 
 		# no dmd.conf installed yet, install a fresh one!
-		echo "[Environment]" > ${DMD_CONF} || die "Error writing to ${DMD_CONF}."
-#		echo ";DFLAGS=-I\"${PREFIX}/include/phobos -L-L${PREFIX}/lib\"" >> ${DMD_CONF} || die "Error writing to ${DMD_CONF}."
-		echo "DFLAGS=-I${PREFIX}/include/tango -L-L${PREFIX}/lib/ -version=Posix -version=Tango" >> ${DMD_CONF} || die "Error writing to ${DMD_CONF}."
+		echo "[Environment]" > ${DMD_CONF} || die 1 "Error writing to ${DMD_CONF}."
+#		echo ";DFLAGS=-I\"${PREFIX}/include/phobos -L-L${PREFIX}/lib\"" >> ${DMD_CONF} || die 1 "Error writing to ${DMD_CONF}."
+		echo "DFLAGS=-I${PREFIX}/include/tango -L-L${PREFIX}/lib/ -version=Posix -version=Tango" >> ${DMD_CONF} || die 1 "Error writing to ${DMD_CONF}."
 	else
 		echo -n "..dmd.conf found: `whereis dmd | tr ' ' '\n' | grep dmd.conf -m 1` -- doing sed magic."
 
 		sed -e 's/DFLAGS=-I.*phobos.*/;&\nDFLAGS=-I"\${PREFIX}\/include\/tango\/ -L-L\${PREFIX}\/lib\/ -version=Posix -version=Tango"/g' `whereis dmd | tr ' ' '\n' | grep -v X | grep -m 1 dmd.conf` > _dmd.conf || die
-		mv _dmd.conf `whereis dmd | tr ' ' '\n' | grep -v X | grep -m 1 dmd.conf` || die # replace old dmd.conf with changed dmd.conf
+		mv _dmd.conf `whereis dmd | tr ' ' '\n' | grep -v X | grep -m 1 dmd.conf` || die 1 # replace old dmd.conf with changed dmd.conf
 	fi
 }
 
 cleanup_tango() {
 	echo "Cleaning up (removing) tango/trunk/..."
 
-	cd ../../ || die "Error while cleaning up."
-	rm -rf tango || die "Error while cleaning up."
+
+	if [ ${TANGO_DOWNLOAD} = 1 ]
+	then
+		if [ ${DMD_INSTALL} = 1 ]
+		then
+			if [ ${ROOT} = 0 ]
+			then
+				mv ${PREFIX} ../../
+			fi
+		fi
+	else
+	fi
+
+	cd ../../ || die 1 "Error while cleaning up."
+	rm -rf tango || die 1 "Error while cleaning up."
 }
 	
 cleanup_dmd() {
 	echo "Cleaning up tango/trunk/dmd/..."
 
-	rm -rf dmd/ || die "Error while cleaning up."
+	rm -rf dmd/ || die 1 "Error while cleaning up."
 
 	if [ ${CLEANALL} = 1 ]
 	then
 		echo "Removing dmd archive..."
-		rm -r ${DMD_FILENAME} || die "Error while removing dmd.zip."
+		rm -r ${DMD_FILENAME} || die 1 "Error while removing dmd.zip."
 	fi
 }
 
@@ -312,16 +321,16 @@ install_dsss() {
 	echo "Installing DSSS to ${PREFIX}..."
 
 	echo "..trying to download archive (this may take a while)..."
-	wget -c ${DSSS_ARCHIVE} || die "Error downloading DSSS binary archive."
+	wget -c ${DSSS_ARCHIVE} || die 1 "Error downloading DSSS binary archive."
 
 	echo "..extracting archive..."
-	unzip -d dsss/ ${DSSS_ARCHIVE##*/} || die "Error extracting DSSS binary archive."
+	unzip -d dsss/ ${DSSS_ARCHIVE##*/} || die 1 "Error extracting DSSS binary archive."
 
 	echo "..copying files over to ${PREFIX}/..."
-	cp -r dsss/dsss-0.9-dmd-gnuWlinux/* ${PREFIX}/ || die "Error copying DSSS to ${PREFIX}."
+	cp -r dsss/dsss-0.9-dmd-gnuWlinux/* ${PREFIX}/ || die 1 "Error copying DSSS to ${PREFIX}."
 
 	echo "..cleaning up..."
-	rm -rf dsss/ || die "Error while cleaning up DSSS temporaries."
+	rm -rf dsss/ || die 1 "Error while cleaning up DSSS temporaries."
 
 	echo "DSSS installed."
 	echo ""
@@ -348,7 +357,12 @@ then
 	PREFIX=$1
 else
 	usage
-	die
+	die 1
+fi
+
+if [ "echo ${PREFIX} | grep '^/'" ]
+then
+	PREFIX=`pwd`${PREFIX}
 fi
 
 # Check for flags
