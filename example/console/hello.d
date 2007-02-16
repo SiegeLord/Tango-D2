f6f24ea3af8d77ca9b4c705f575c1a8eb3a34e3f
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-02-16 23:30:56 +0000

    doc fix

--- example/console/hello.d
+++ example/console/hello.d
@@ -3,12 +3,10 @@
         Hello World using tango.io
 
         This illustrates bare console output, with no fancy formatting. 
-        One could use the Print module instead, which provides printf()
-        formatting support.
 
-        Console I/O in Mango is UTF-8 across both linux and Win32. The
+        Console I/O in Tango is UTF-8 across both linux and Win32. The
         conversion between various unicode representations is handled
-        by higher level constructs, including Print, Stdin, and Stdout.
+        by higher level constructs, such as Stdout and Stderr
 
         Note that Cerr is tied to the console error output, and Cin is
         tied to the console input. 
