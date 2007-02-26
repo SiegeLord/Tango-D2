e494cacf97620001ab40edef9360924c79d2f3ae
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-02-26 22:23:24 +0000

    updated to new toUtf8() args

--- example/text/localetime.d
+++ example/text/localetime.d
@@ -13,5 +13,7 @@ private import tango.text.locale.Core;
 
 void main ()
 {
-        Cout (DateTime.now.toUtf8 ("ddd, dd MMMM yyyy HH:mm:ss z")).newline;
+        char[64] tmp = void;
+
+        Cout (DateTime.now.toUtf8 (tmp, "ddd, dd MMMM yyyy HH:mm:ss z")).newline;
 }
