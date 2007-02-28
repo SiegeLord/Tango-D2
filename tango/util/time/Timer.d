6f962f61083740af47fc593e71a0f2df01a5aaf2
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-02-28 01:46:19 +0000

    Added a short duration timer

---
+++ tango/util/time/Timer.d
@@ -0,0 +1,154 @@
+/*******************************************************************************
+
+        copyright:      Copyright (c) 2007 Kris Bell. All rights reserved
+
+        license:        BSD style: $(LICENSE)
+      
+        version:        Feb 2007: Initial release
+        
+        author:         Kris
+
+*******************************************************************************/
+
+module tango.util.time.Timer;
+
+private import tango.core.Type : Interval;
+
+/*******************************************************************************
+
+*******************************************************************************/
+
+version (Win32)
+{
+        private extern(Windows) 
+        {
+        int QueryPerformanceCounter   (ulong *count);
+        int QueryPerformanceFrequency (ulong *frequency);
+        }
+}
+
+/*******************************************************************************
+
+        Timer for measuring small intervals, such as the duration of a 
+        subroutine or other element of code.
+        ---
+        Timer timer;
+
+        timer.start;
+
+        // do something
+        // ...
+
+        Interval i = timer.stop;
+        ---
+
+        The measured interval is in units of seconds, using floating-
+        point to represent fractions. This approach is more flexible 
+        than integer arithmetic since it migrates trivially to more
+        capable timer hardware (there no implicit granularity to the
+        measurable intervals, except the limits of fp representation)
+
+        Timer is accurate only to the extent of what the underlying OS
+        supports. On linux systems, this accuracy is currently 1 ms at 
+        best. Win32 is generally more accurate.
+
+*******************************************************************************/
+
+public struct Timer
+{
+        private Interval started;
+
+        version (Win32)
+        {
+                private static Interval multiplier;
+                private static ulong    timerStart;
+        }
+
+        /***********************************************************************
+                
+                Start the timer
+
+        ***********************************************************************/
+        
+        void start ()
+        {
+                started = timer;
+        }
+
+        /***********************************************************************
+                
+                Stop the timer and return elapsed duration since start()
+
+        ***********************************************************************/
+        
+        Interval stop ()
+        {
+                return timer - started;
+        }
+
+        /***********************************************************************
+                
+                Setup timing information for later use
+
+        ***********************************************************************/
+
+        static this()
+        {
+                version (Win32)
+                {
+                        ulong freq;
+
+                        if (! QueryPerformanceFrequency (&freq))
+                              throw new Exception ("high-resolution timer is not available");
+                        
+                        QueryPerformanceCounter (&timerStart);
+                        multiplier = 1.0 / freq;       
+                }
+        }
+
+        /***********************************************************************
+                
+                Return the current time as an Interval
+
+        ***********************************************************************/
+
+        private static Interval timer ()
+        {
+                version (Posix)       
+                {
+                        timeval tv;
+                        if (gettimeofday (&tv, null))
+                            throw new Exception ("Timer :: linux timer is not available");
+
+                        return (cast(Interval) tv.tv_sec) + tv.tv_usec / 1_000_000.0;
+                }
+
+                version (Win32)
+                {
+                        ulong now;
+
+                        QueryPerformanceCounter (&now);
+                        return (now - timerStart) * multiplier;
+                }
+        }
+}
+
+
+/*******************************************************************************
+
+*******************************************************************************/
+
+debug (Timer)
+{
+        import tango.io.Stdout;
+
+        void main() 
+        {
+                Timer t;
+                t.start;
+
+                for (int i=0; i < 10_000_000; ++i)
+                    {}
+                Stdout.format ("{:f9}", t.stop).newline;
+        }
+}
