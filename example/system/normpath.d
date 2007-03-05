df30768569336462948345407495301b8c626031
larsivi <larsivi@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-03-05 19:05:54 +0000

    Added windows versions of paths

--- example/system/normpath.d
+++ example/system/normpath.d
@@ -13,6 +13,7 @@ import tango.util.PathUtil;
 
 int main()
 {
+version (Posix) {
     Stdout(normalize ( "/foo/../john")).newline;
     Stdout(normalize ( "foo/../john")).newline;    
     Stdout(normalize ( "foo/bar/..")).newline;    
@@ -27,6 +28,24 @@ int main()
     Stdout(normalize ( "../../../foo/bar")).newline;
     Stdout("** Should now throw exception as the following path is invalid for normalization.").newline;
     Stdout(normalize ( "/../../../foo/bar")).newline;
+}
+version (Windows) {
+    Stdout(normalize ( "C:\\foo\\..\\john")).newline;
+    Stdout(normalize ( "foo\\..\\john")).newline;    
+    Stdout(normalize ( "foo\\bar\\..")).newline;    
+    Stdout(normalize ( "foo\\bar\\..\\john")).newline;
+    Stdout(normalize ( "foo\\bar\\doe\\..\\..\\john")).newline;
+    Stdout(normalize ( "foo\\bar\\doe\\..\\..\\john\\..\\bar")).newline;
+    Stdout(normalize ( ".\\foo\\bar\\doe")).newline;
+    Stdout(normalize ( ".\\foo\\bar\\doe\\..\\..\\john\\..\\bar")).newline;
+    Stdout(normalize ( ".\\foo\\bar\\..\\..\\john\\..\\bar")).newline;
+    Stdout(normalize ( "foo\\bar\\.\\doe\\..\\..\\john")).newline;
+    Stdout(normalize ( "..\\..\\foo\\bar\\.\\doe\\..\\..\\john")).newline;
+    Stdout(normalize ( "..\\..\\..\\foo\\bar")).newline;
+    Stdout("** Should now throw exception as the following path is invalid for normalization.").newline;
+    Stdout(normalize ( "C:\\..\\..\\..\\foo\\bar")).newline;
+}
+
 
     return 0;
 }
