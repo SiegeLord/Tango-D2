5f1a8b9006bfa49d3af6be5ee78dfc19152a2519
larsivi <larsivi@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-03-10 15:46:10 +0000

    First iteration of tar.gz creating script

---
+++ install/snapshot/snapshotbuilder.d
@@ -0,0 +1,188 @@
+import tango.text.Properties;
+import tango.sys.Process;
+import tango.io.FileSystem;
+import tango.io.FilePath;
+import tango.io.FileConst;
+import tango.text.stream.LineIterator;
+
+import tango.io.Stdout;
+
+import tango.util.time.Date;
+import tango.util.time.Utc;
+
+char[] workdir;
+char[] wcdir;
+char[] repourl;
+char[] projname;
+char[] buildcmd;
+char[] chmodfiles;
+char[] rmpaths;
+
+int main(char[][] args)
+{
+    if (args.length < 2) {
+        Stderr("Provide a path to a config file").newline;
+        return 1;
+    }
+
+    // Read config
+    void configReader(char[] name, char[]value)
+    {
+        switch (name) {
+        case "workdir":
+            workdir = value;
+            break;
+        case "wcdir":
+            wcdir = value;
+            break;
+        case "repourl":
+            repourl = value;
+            break;
+        case "projname":
+             projname = value;
+             break;
+        case "buildcmd":
+             buildcmd = value;
+             break;
+        case "chmodfiles":
+             chmodfiles = value;
+             break;
+        case "rmpaths":
+             rmpaths = value;
+             break;
+          default:
+            break;
+        }
+
+        return;
+    }
+
+    auto props = new Properties!(char);
+    props.load(new FilePath(args[1]), &configReader);
+
+
+    // Enter work directory
+    FileSystem.setDirectory(workdir);
+    // Check out repository if working copy don't exist, update it if it do
+    auto wc = new FilePath(wcdir);
+    if (!wc.exists) {
+        Stdout("!! Checking out working copy from repository").newline;
+        auto svnco = new Process("svn co " ~ repourl ~ " " ~ wcdir, null);
+        svnco.execute();
+        auto result = svnco.wait();
+        if (result.reason != Process.Result.Exit) {
+            Stderr("Was not able to check out working copy from repository").newline;
+            return 1;
+        }
+    }
+    else {
+        Stdout("!! Updating working copy").newline;
+        FileSystem.setDirectory(wcdir);
+        auto svnup = new Process("svn up", null);
+        svnup.execute();
+        auto result = svnup.wait();
+        if (result.reason != Process.Result.Exit) {
+            Stderr("Was not able to update working copy").newline;
+            return 1;
+        }
+    }
+
+    // Make sure we're still in work directory
+    FileSystem.setDirectory(workdir);
+
+    // export wc to dir to be packaged, will need to have the name of the package
+
+    Stdout("!! Exporting from working copy for packaging").newline;
+    Date date;
+    date.set(Utc.local);
+    char[] datestr; 
+    datestr = Stdout.layout.sprint(new char[10], "{}{:2}{:2}", date.year, date.month, date.day);
+    char[] packdir = projname ~ "-src-SNAPSHOT-" ~ datestr;
+
+    auto pd = new FilePath(packdir, true);
+    if (pd.exists) {
+        Stdout("!! Packaging dir already exists, removing before recreating").newline;
+        auto rmdir = new Process("rm -rf " ~ packdir, null);
+        rmdir.execute();
+        auto result = rmdir.wait();
+        if (result.reason != Process.Result.Exit) {
+            Stderr("Was not able to remove old package dir").newline;
+            return 1;
+        }
+    }
+
+    auto svnexp = new Process("svn export " ~ wcdir ~ " " ~ packdir, null);
+    svnexp.execute();
+    auto result = svnexp.wait();
+    if (result.reason != Process.Result.Exit) {
+        Stderr("Was not able to export working copy").newline;
+        return 1;
+    }
+ 
+    // enter packagedir/lib
+    Stdout("!! Generate files").newline;
+    FileSystem.setDirectory(packdir ~ FileConst.PathSeparatorString ~ "lib");
+
+    // run build-*.*
+    auto bld = new Process(buildcmd, null);
+    bld.execute();
+    foreach(line; new LineIterator!(char)(bld.stderr))
+    {
+        Stderr(line).newline;
+    }
+    result = bld.wait();
+    if (result.reason != Process.Result.Exit) {
+        Stderr("Was not able to build ").newline;
+        return 1;
+    }
+ 
+    // enter package root dir
+    FileSystem.setDirectory(workdir ~ FileConst.PathSeparatorString ~ packdir);
+
+    // change access to generated files
+    Stdout("!! Changing access rights").newline;
+    auto chmod = new Process("chmod 644 " ~ chmodfiles, null);
+    chmod.execute();
+    foreach(line; new LineIterator!(char)(chmod.stderr))
+    {
+        Stderr(line).newline;
+    }
+    result = chmod.wait();
+    if (result.reason != Process.Result.Exit) {
+        Stderr("Was not able to change access rights").newline;
+        return 1;
+    }
+ 
+    // remove patches/ , install/win32/, docs and maybe more
+    Stdout("!! Removing " ~ rmpaths).newline;
+    auto rm = new Process("rm -rf " ~ rmpaths, null);
+    rm.execute();
+    foreach(line; new LineIterator!(char)(rm.stderr))
+    {
+        Stderr(line).newline;
+    }
+    result = rm.wait();
+    if (result.reason != Process.Result.Exit) {
+        Stderr("Was not able to remove paths").newline;
+        return 1;
+    }
+ 
+    // enter ..
+    FileSystem.setDirectory(workdir);
+
+    // create zip, etc
+    Stdout("!! Creating .tar.gz").newline;
+    auto targz = new Process("tar czf " ~ packdir ~ ".tar.gz " ~ packdir, null);
+    targz.execute();
+    foreach(line; new LineIterator!(char)(targz.stderr))
+    {
+        Stderr(line).newline;
+    }
+    result = targz.wait();
+    if (result.reason != Process.Result.Exit) {
+        Stderr("Was not able to remove paths").newline;
+        return 1;
+    }
+ 
+    return 0;
+}
