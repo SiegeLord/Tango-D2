406071bace899f9e0e6ba3502e2397f25e5ab8f3
sean <sean@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-04-12 19:40:57 +0000

    Reverting to previous impl.  It appears the osx docs don't match the headers.

--- tango/stdc/time.d
+++ tango/stdc/time.d
@@ -13,31 +13,37 @@ private import tango.stdc.stddef;
 
 extern (C):
 
-struct tm
+version( Win32 )
 {
-    int     tm_sec;     // seconds after the minute [0-60]
-    int     tm_min;     // minutes after the hour [0-59]
-    int     tm_hour;    // hours since midnight [0-23]
-    int     tm_mday;    // day of the month [1-31]
-    int     tm_mon;     // months since January [0-11]
-    int     tm_year;    // years since 1900
-    int     tm_wday;    // days since Sunday [0-6]
-    int     tm_yday;    // days since January 1 [0-365]
-    int     tm_isdst;   // Daylight Savings Time flag
-  version( Win32 )
-  {
-    // nothing else for Win32
-  }
-  else version( darwin )
-  {
-    char*   tm_zone;    // timezone abbreviation
-    c_long  tm_gmtoff;  // offset from CUT in seconds
-  }
-  else
-  {
-    c_long  tm_gmtoff;  // offset from CUT in seconds
-    char*   tm_zone;    // timezone abbreviation
-  }
+    struct tm
+    {
+        int     tm_sec;     // seconds after the minute - [0, 60]
+        int     tm_min;     // minutes after the hour - [0, 59]
+        int     tm_hour;    // hours since midnight - [0, 23]
+        int     tm_mday;    // day of the month - [1, 31]
+        int     tm_mon;     // months since January - [0, 11]
+        int     tm_year;    // years since 1900
+        int     tm_wday;    // days since Sunday - [0, 6]
+        int     tm_yday;    // days since January 1 - [0, 365]
+        int     tm_isdst;   // Daylight Saving Time flag
+    }
+}
+else
+{
+    struct tm
+    {
+        int     tm_sec;     // seconds after the minute [0-60]
+        int     tm_min;     // minutes after the hour [0-59]
+        int     tm_hour;    // hours since midnight [0-23]
+        int     tm_mday;    // day of the month [1-31]
+        int     tm_mon;     // months since January [0-11]
+        int     tm_year;    // years since 1900
+        int     tm_wday;    // days since Sunday [0-6]
+        int     tm_yday;    // days since January 1 [0-365]
+        int     tm_isdst;   // Daylight Savings Time flag
+        c_long  tm_gmtoff;  // offset from CUT in seconds
+        char*   tm_zone;    // timezone abbreviation
+    }
 }
 
 alias int time_t;
