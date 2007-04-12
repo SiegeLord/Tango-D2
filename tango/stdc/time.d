3b6c9bacc4afb8bf7d4d3d8280465edabbfca236
sean <sean@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-04-12 19:35:12 +0000

    Apparently, the last two members of struct tm are reversed on darwin vs. linux.  Fixed.

--- tango/stdc/time.d
+++ tango/stdc/time.d
@@ -13,37 +13,31 @@ private import tango.stdc.stddef;
 
 extern (C):
 
-version( Win32 )
+struct tm
 {
-    struct tm
-    {
-        int     tm_sec;     // seconds after the minute - [0, 60]
-        int     tm_min;     // minutes after the hour - [0, 59]
-        int     tm_hour;    // hours since midnight - [0, 23]
-        int     tm_mday;    // day of the month - [1, 31]
-        int     tm_mon;     // months since January - [0, 11]
-        int     tm_year;    // years since 1900
-        int     tm_wday;    // days since Sunday - [0, 6]
-        int     tm_yday;    // days since January 1 - [0, 365]
-        int     tm_isdst;   // Daylight Saving Time flag
-    }
-}
-else
-{
-    struct tm
-    {
-        int     tm_sec;     // seconds after the minute [0-60]
-        int     tm_min;     // minutes after the hour [0-59]
-        int     tm_hour;    // hours since midnight [0-23]
-        int     tm_mday;    // day of the month [1-31]
-        int     tm_mon;     // months since January [0-11]
-        int     tm_year;    // years since 1900
-        int     tm_wday;    // days since Sunday [0-6]
-        int     tm_yday;    // days since January 1 [0-365]
-        int     tm_isdst;   // Daylight Savings Time flag
-        c_long  tm_gmtoff;  // offset from CUT in seconds
-        char*   tm_zone;    // timezone abbreviation
-    }
+    int     tm_sec;     // seconds after the minute [0-60]
+    int     tm_min;     // minutes after the hour [0-59]
+    int     tm_hour;    // hours since midnight [0-23]
+    int     tm_mday;    // day of the month [1-31]
+    int     tm_mon;     // months since January [0-11]
+    int     tm_year;    // years since 1900
+    int     tm_wday;    // days since Sunday [0-6]
+    int     tm_yday;    // days since January 1 [0-365]
+    int     tm_isdst;   // Daylight Savings Time flag
+  version( Win32 )
+  {
+    // nothing else for Win32
+  }
+  else version( darwin )
+  {
+    char*   tm_zone;    // timezone abbreviation
+    c_long  tm_gmtoff;  // offset from CUT in seconds
+  }
+  else
+  {
+    c_long  tm_gmtoff;  // offset from CUT in seconds
+    char*   tm_zone;    // timezone abbreviation
+  }
 }
 
 alias int time_t;
