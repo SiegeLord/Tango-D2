935065fb2563e08a38853afdffd6689c310d147a
Jim Panic <Jim Panic@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-04-15 20:45:04 +0000

    Fixed install-dmd.sh (Ya rly this time)

--- lib/install-dmd.sh
+++ lib/install-dmd.sh
@@ -31,6 +31,8 @@ SETPREFIX=0
 UNINSTALL=0
 REPLACE_PHOBOS=0
 
+PREFIX="/usr/local"
+
 while [ "$#" != "0" ]
 do
     if [ "$1" = "--inplace" ]
@@ -73,8 +75,6 @@ else
 	if [ "$REPLACE_PHOBOS" = "1" ]
 	then
 		PREFIX="$PHOBOS_DIR/.."
-	else
-		PREFIX="/usr/local"	
 	fi
 fi
 DMD_VER="`dmd | head -n 1 | awk '{print $5}'`"
@@ -89,16 +89,20 @@ fi
 # If uninstalling, do that now
 if [ "$UNINSTALL" = "1" ]
 then
-    if [ ! -e "$PHOBOS_DIR/libtango.a" ]
+    if [ ! -e "$PREFIX/lib/libphobos.a.phobos" ]
     then
-        die "tango does not appear to be installed!" 3
+        #die "tango does not appear to be installed!" 3 # This is actually bullshit, because there are also installations without any libphobos.a.phobos :P
+		rm -f $PREFIX/lib/libphobos.a
+		rm -f $PREFIX/lib/libtango.a
+	else
+		if [ "$INPLACE" = "0" ]
+		then
+			rm -rf $PHOBOS_DIR/libphobos.a $PREFIX/import/$DMD_VER/object.d
+			mv     $PHOBOS_DIR/libphobos.a.phobos $PHOBOS_DIR/libphobos.a
+			mv     $PREFIX/import/$DMD_VER/object.d.phobos $PREFIX/import/$DMD_VER/object.d
+		fi
     fi
-    if [ "$INPLACE" = "0" ]
-    then
-        rm -rf $PHOBOS_DIR/libphobos.a $PREFIX/import/$DMD_VER/object.d
-        mv $PREFIX/import/$DMD_VER/object.d.phobos $PREFIX/import/$DMD_VER/object.d
-        mv $PHOBOS_DIR/libphobos.a.phobos $PHOBOS_DIR/libphobos.a
-    fi
+
     die "Done!" 0
 fi
 
@@ -126,9 +130,14 @@ if [ "$INPLACE" = "0" ]
 then
     echo 'Copying files...'
     mkdir -p $PREFIX/import/$DMD_VER || die "Failed to create import/$DMD_VER (maybe you need root privileges?)" 5
-	mkdir -p $PREFIX/lib/ || die "Failed to create $PREFIX/lib (maybe you need root privileges?)" 6
+	mkdir -p $PREFIX/lib/ || die "Failed to create $PREFIX/lib (maybe you need root privileges?)" 5
+	mkdir -p $PREFIX/bin/ || die "Failed to create $PREFIX/bin" 5
     cp -pRvf libphobos.a $PREFIX/lib/ || die "Failed to copy libraries" 7
     cp -pRvf ../object.di $PREFIX/import/$DMD_VER/object.di || die "Failed to copy source" 8
+	cat > $PREFIX/bin/dmd.conf <<EOF
+[Environment]
+DFLAGS=-I$PREFIX/import/ -version=Tango -version=Posix
+EOF
 fi
 
 die "Done!" 0
