3031d96936f36dd931a226433695399257783910
sean <sean@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-04-21 16:39:38 +0000

    Fix for ticket #424.

--- lib/common/tango/core/Thread.d
+++ lib/common/tango/core/Thread.d
@@ -180,7 +180,7 @@ else version( Posix )
                 obj.m_isRunning = false;
             }
 
-            pthread_cleanup cleanup;
+            mixin pthread_cleanup cleanup;
             cleanup.push( &thread_cleanupHandler, cast(void*) obj );
 
             // NOTE: For some reason this does not always work for threads.
