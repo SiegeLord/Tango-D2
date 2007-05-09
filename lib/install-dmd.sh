fd6aaed610f60804241dafb390778e465c4980ae
larsivi <larsivi@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-05-09 20:39:33 +0000

    Changed around on the uninstall, removed INPLACE option

--- lib/install-dmd.sh
+++ lib/install-dmd.sh
@@ -16,8 +16,6 @@ die() {
 usage() {
     echo 'Usage: install-dmd.sh [--inplace] [--prefix <install prefix>]
 Options:
-  --inplace: Don'\''t install anywhere, just keep the installation in-place.
-             (Not recommended, doesn'\''t work without -I)
   --prefix: Install to the specified prefix.
   --uninstall: Uninstall tango, switch back to standard phobos.'
     exit 0
@@ -26,7 +24,6 @@ Options:
 cd "`dirname $0`"
 
 # 0) Parse arguments
-INPLACE=0
 SETPREFIX=0
 UNINSTALL=0
 REPLACE_PHOBOS=0
@@ -35,10 +32,7 @@ PREFIX="/usr/local"
 
 while [ "$#" != "0" ]
 do
-    if [ "$1" = "--inplace" ]
-    then
-        INPLACE=1
-    elif [ "$1" = "--prefix" ]
+    if [ "$1" = "--prefix" ]
     then
         SETPREFIX=1
         shift
@@ -83,45 +77,38 @@ else
 	fi
 fi
 
-if [ "$INPLACE" = "1" -a \
-     "$SETPREFIX" = "1" ]
-then
-    die "Cannot both set a prefix and do an in-place install." 2
-fi
-
 # If uninstalling, do that now
 if [ "$UNINSTALL" = "1" ]
 then
-    if [ ! -e "$PREFIX/lib/libphobos.a.phobos" ]
+    if [ -e "$PHOBOS_DIR/libphobos.a.phobos" ]
     then
-		rm -f $PREFIX/lib/libphobos.a
-		rm -f $PREFIX/lib/libtango.a
-        if [ -e "$PREFIX/import/v1.012" ]
-        then
-            rm -rf $PREFIX/import/v1.012
-        fi
-        if [ -e "$PREFIX/import/object.di" ]
-        then
-            rm -f $PREFIX/import/object.di
-            rm -rf $PREFIX/import/std
-            rm -rf $PREFIX/import/tango
-        fi
-        if [ -e "$PREFIX/import/tango" ]
-        then
-            rm -rf $PREFIX/import/tango
-        fi
-	else
-		if [ "$INPLACE" = "0" ]
-		then
-			rm -f  $PHOBOS_DIR/libphobos.a $PREFIX/import/object.di
-			mv     $PHOBOS_DIR/libphobos.a.phobos $PHOBOS_DIR/libphobos.a
-            if [ -e "$PREFIX/import/object.d.phobos" ]
-            then
-			    mv     $PREFIX/import/object.d.phobos $PREFIX/import/object.d
-            fi
-		fi
+        mv     $PHOBOS_DIR/libphobos.a.phobos $PHOBOS_DIR/libphobos.a
     fi
-
+    if [ -e "$PREFIX/import/object.d.phobos" ]
+    then
+        mv     $PREFIX/import/object.d.phobos $PREFIX/import/object.d
+    fi
+    # Tango 0.97 installed to this dir
+    if [ -e "$PREFIX/import/v1.012" ]
+    then
+        rm -rf $PREFIX/import/v1.012
+    fi
+    # Some svn versions installed here
+    if [ -e "$PREFIX/import/object.di" ]
+    then
+        rm -f $PREFIX/import/object.di
+        rm -rf $PREFIX/import/std
+        rm -rf $PREFIX/import/tango
+    fi
+    # Since Tango 0.98
+    if [ -e "$PREFIX/import/tango" ]
+    then
+        rm -rf $PREFIX/import/tango
+    fi
+    if [ -e "$PREFIX/lib/libtango.a" ]
+    then
+		rm -f $PREFIX/lib/libtango.a
+    fi
     die "Done!" 0
 fi
 
@@ -147,18 +134,15 @@ then
 fi
 
 # Install ...
-if [ "$INPLACE" = "0" ]
-then
-    echo 'Copying files...'
-    mkdir -p $PREFIX/import/tango || die "Failed to create import (maybe you need root privileges?)" 5
-	mkdir -p $PREFIX/lib/ || die "Failed to create $PREFIX/lib (maybe you need root privileges?)" 5
-	mkdir -p $PREFIX/bin/ || die "Failed to create $PREFIX/bin" 5
-    cp -pRvf libphobos.a $PREFIX/lib/ || die "Failed to copy libraries" 7
-    cp -pRvf ../object.di $PREFIX/import/tango/object.di || die "Failed to copy source" 8
-	cat > $PREFIX/bin/dmd.conf <<EOF
+echo 'Copying files...'
+mkdir -p $PREFIX/import/tango || die "Failed to create import (maybe you need root privileges?)" 5
+mkdir -p $PREFIX/lib/ || die "Failed to create $PREFIX/lib (maybe you need root privileges?)" 5
+mkdir -p $PREFIX/bin/ || die "Failed to create $PREFIX/bin" 5
+cp -pRvf libphobos.a $PREFIX/lib/ || die "Failed to copy libraries" 7
+cp -pRvf ../object.di $PREFIX/import/tango/object.di || die "Failed to copy source" 8
+cat > $PREFIX/bin/dmd.conf <<EOF
 [Environment]
 DFLAGS=-I$PREFIX/import/tango -version=Tango -version=Posix -L-L"%@P%/../lib"
 EOF
-fi
 
 die "Done!" 0
