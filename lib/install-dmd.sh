534a7b03bce3b6b9e28e159a1869b58262156d36
larsivi <larsivi@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-05-11 19:10:29 +0000

    Retain existing Tango enabled dmd.conf as is, preserve phobos dmd.conf, and reinstate during uninstall

--- lib/install-dmd.sh
+++ lib/install-dmd.sh
@@ -80,6 +80,7 @@ fi
 # If uninstalling, do that now
 if [ "$UNINSTALL" = "1" ]
 then
+    # revert to phobos if earlier evidence of existense is found
     if [ -e "$PHOBOS_DIR/libphobos.a.phobos" ]
     then
         mv     $PHOBOS_DIR/libphobos.a.phobos $PHOBOS_DIR/libphobos.a
@@ -87,6 +88,11 @@ then
     if [ -e "$PREFIX/import/object.d.phobos" ]
     then
         mv     $PREFIX/import/object.d.phobos $PREFIX/import/object.d
+    fi
+    if [ -e "$PREFIX/bin/dmd.conf.phobos" ]
+    then
+        mv   $PREFIX/bin/dmd.conf $PFEFIX/bin/dmd.conf.tango
+        mv   $PREFIX/bin/dmd.conf.phobos $PREFIX/bin/dmd.conf
     fi
     # Tango 0.97 installed to this dir
     if [ -e "$PREFIX/import/v1.012" ]
@@ -128,6 +134,13 @@ then
     fi
 fi
 
+create_dmd_conf() {
+    cat > $PREFIX/bin/dmd.conf <<EOF
+[Environment]
+DFLAGS=-I$PREFIX/import/tango -version=Tango -version=Posix -L-L"%@P%/../lib"
+EOF
+}
+
 # Install ...
 echo 'Copying files...'
 mkdir -p $PREFIX/import/tango || die "Failed to create import (maybe you need root privileges?)" 5
@@ -135,9 +148,18 @@ mkdir -p $PREFIX/lib/ || die "Failed to create $PREFIX/lib (maybe you need root 
 mkdir -p $PREFIX/bin/ || die "Failed to create $PREFIX/bin" 5
 cp -pRvf libphobos.a $PREFIX/lib/ || die "Failed to copy libraries" 7
 cp -pRvf ../object.di $PREFIX/import/tango/object.di || die "Failed to copy source" 8
-cat > $PREFIX/bin/dmd.conf <<EOF
-[Environment]
-DFLAGS=-I$PREFIX/import/tango -version=Tango -version=Posix -L-L"%@P%/../lib"
-EOF
+if [ ! -e "$PREFIX/bin/dmd.conf" ]
+then
+    create_dmd_conf
+else
+    # Is it a phobos conf ?
+    if [ ! "`grep -version=Tango $PREFIX/bin/dmd.conf`" ]
+    then
+        mv $PREFIX/bin/dmd.conf $PREFIX/bin/dmd.conf.phobos 
+        create_dmd_conf
+    else
+        echo 'Tango enabled dmd.conf, assume it is working and leave it as is'
+    fi
+fi
 
 die "Done!" 0
