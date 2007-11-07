d5ab7ca1924a66c4f26bd2b93bf56bcbb24265d7
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-11-07 05:27:50 +0000

    added these as file-stream compositions

---
+++ tango/io/stream/RawFileStream.d
@@ -0,0 +1,126 @@
+/*******************************************************************************
+
+        copyright:      Copyright (c) 2007 Kris Bell. All rights reserved
+
+        license:        BSD style: $(LICENSE)
+
+        version:        Initial release: Nov 2007
+
+        author:         Kris
+
+*******************************************************************************/
+
+module tango.io.stream.RawFileStream;
+
+public  import tango.io.FileConduit;
+
+private import tango.io.stream.FileStream,
+               tango.io.stream.DataStream;
+
+/*******************************************************************************
+
+        Composes a seekable file with buffered binary input. A seek causes
+        the input buffer to be cleared
+
+*******************************************************************************/
+
+class RawFileInput : DataInput
+{
+        private FileConduit conduit;
+
+        public alias FileConduit.Seek.Anchor Anchor;
+
+        /***********************************************************************
+
+                compose a FileStream              
+
+        ***********************************************************************/
+
+        this (char[] path, FileConduit.Style style = FileConduit.ReadExisting)
+        {
+                super (conduit = new FileInput (path, style));
+        }
+
+        /***********************************************************************
+
+                Set the file seek position to the specified offset from 
+                the given anchor, and clear the input buffer
+        
+        ***********************************************************************/
+
+        final long seek (long offset, Anchor anchor = Anchor.Begin)
+        {
+                host.clear;
+                return conduit.seek (offset, anchor);
+        }
+
+        /***********************************************************************
+
+                Return the underling conduit
+
+        ***********************************************************************/
+
+        final FileConduit file ()
+        {       
+                return conduit;
+        }
+}
+
+
+/*******************************************************************************
+       
+        Composes a seekable file with buffered binary output. A seek causes
+        the output buffer to be flushed first
+
+*******************************************************************************/
+
+class RawFileOutput : DataOutput
+{
+        private FileConduit conduit;
+
+        public alias FileConduit.Seek.Anchor Anchor;
+
+        /***********************************************************************
+
+                compose a FileStream              
+
+        ***********************************************************************/
+
+        this (char[] path, FileConduit.Style style = FileConduit.WriteCreate)
+        {
+                super (conduit = new FileOutput (path, style));
+        }
+
+        /***********************************************************************
+
+                Set the file seek position to the specified offset from 
+                the given anchor, after flushing the output buffer
+        
+        ***********************************************************************/
+
+        final long seek (long offset, Anchor anchor = Anchor.Begin)
+        {
+                host.flush;
+                return conduit.seek (offset, anchor);
+        }
+
+        /***********************************************************************
+
+                Return the underling conduit
+
+        ***********************************************************************/
+
+        final FileConduit file ()
+        {       
+                return conduit;
+        }
+}
+
+
+/*******************************************************************************
+
+*******************************************************************************/
+
+debug (UnitTest)
+{
+}
