62e6dd9d048d563eb545923dbe2d276ee46a6fc9
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-11-10 18:42:09 +0000

    ach, there were more changes

--- tango/io/FilePath.d
+++ tango/io/FilePath.d
@@ -1559,8 +1559,8 @@ class FilePath : PathView
                 {
                         int             ret;
                         DIR*            dir;
-                        dirent          entry;
-                        //dirent*         pentry;
+                        dirent*         entry;
+                        //dirent*       pentry;
                         stat_t          sbuf;
                         char[]          prefix;
                         char[]          sfnbuf;
@@ -1578,8 +1578,8 @@ class FilePath : PathView
                         // prepare our filename buffer
                         sfnbuf = prefix.dup;
 
-                          while ((entry = tango.stdc.posix.dirent.readdir(dir)) != null)
-//                        while (tango.stdc.posix.dirent.readdir_r(dir, &entry, &pentry) == 0)
+                        while ((entry = tango.stdc.posix.dirent.readdir(dir)) != null)
+//                      while (tango.stdc.posix.dirent.readdir_r(dir, &entry, &pentry) == 0)
                               {
                               auto len = tango.stdc.string.strlen (entry.d_name.ptr);
                               auto str = entry.d_name.ptr [0 .. len];
