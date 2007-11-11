12f305a27dafbba6792af6129b566d1622c8a143
sean <sean@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-11-11 16:17:57 +0000

    Oops.  Forgot to add this batch file during my library name change commit.

---
+++ lib/build-win32.bat
@@ -0,0 +1,16 @@
+pushd ..\tango\sys\win32
+copy /y Macros.di   Macros.d
+copy /y Process.di  Process.d
+copy /y Types.di    Types.d
+copy /y UserGdi.di  UserGdi.d
+
+dmd -I..\..\.. -c -inline -release -O Macros.d Process.d Types.d UserGdi.d
+lib -c -n dtango-arch-win32.lib Macros.obj Process.obj Types.obj UserGdi.obj
+
+move /y dtango-arch-win32.lib ..\..\..\lib\.
+
+del Macros.d    Macros.obj
+del Process.d   Process.obj
+del Types.d     Types.obj
+del UserGdi.d   UserGdi.obj
+popd
