8069ea98dde907a3923f1bb92c4e025bf15ad738
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-12-08 07:25:10 +0000

    added convenience stream importer

---
+++ tango/stream.d
@@ -0,0 +1,31 @@
+/*******************************************************************************
+
+        copyright:      Copyright (c) 2007 Kris Bell. All rights reserved
+
+        license:        BSD style: $(LICENSE)
+        
+        version:        Dec 2007: Initial release
+
+        author:         Kris
+
+        Convenience module to import streams 
+
+*******************************************************************************/
+
+module tango.stream;
+
+public  import
+        tango.io.stream.BufferStream,
+        tango.io.stream.DataFileStream,
+        tango.io.stream.DataStream,
+        tango.io.stream.DigestStream,
+        tango.io.stream.EndianStream,
+        tango.io.stream.FileStream,
+        tango.io.stream.FormatStream,
+        tango.io.stream.GreedyStream,
+        tango.io.stream.LineStream,
+        tango.io.stream.MapStream,
+        tango.io.stream.SnoopStream,
+        tango.io.stream.TextFileStream,
+        tango.io.stream.TypedStream,
+        tango.io.stream.UtfStream;
