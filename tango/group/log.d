cdd8ac26c79b5f6cc1853195e1b3915ea3168fdb
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-12-08 22:36:27 +0000

    moved these back to tango.group

---
+++ tango/group/log.d
@@ -0,0 +1,29 @@
+/*******************************************************************************
+
+        copyright:      Copyright (c) 2007 Kris Bell. All rights reserved
+
+        license:        BSD style: $(LICENSE)
+        
+        version:        Dec 2007: Initial release
+
+        author:         Kris
+
+        Convenience module to import tango.util.log modules 
+
+*******************************************************************************/
+
+module tango.group.log;
+
+public import tango.log.Log;
+public import tango.log.Logger;
+public import tango.log.Hierarchy;
+public import tango.log.DateLayout;
+public import tango.log.EventLayout;
+public import tango.log.Log4JLayout;
+public import tango.log.FileAppender;
+public import tango.log.MailAppender;
+public import tango.log.SocketAppender
+public import tango.log.ConsoleAppender;
+public import tango.log.RollingFileAppender;
+                
+
