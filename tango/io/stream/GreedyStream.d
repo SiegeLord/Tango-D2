5956c05e58a5b7d8b57109e16a1a5db62e1a68b1
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-12-09 18:02:13 +0000

    added readExact & writeExact

--- tango/io/stream/GreedyStream.d
+++ tango/io/stream/GreedyStream.d
@@ -14,6 +14,73 @@ module tango.io.stream.GreedyStream;
 
 private import tango.io.Conduit;
 
+
+/*******************************************************************************
+
+        A conduit filter that ensures its input is read in full         
+
+*******************************************************************************/
+
+class GreedyInput : InputFilter
+{
+        /***********************************************************************
+
+                Propagate ctor to superclass
+
+        ***********************************************************************/
+
+        this (InputStream stream)
+        {
+                super (stream);
+        }
+
+        /***********************************************************************
+
+                Fill the provided array. Returns the number of bytes
+                actually read, which will be less that dst.length when
+                Eof has been reached, and then Eof thereafter
+
+        ***********************************************************************/
+
+        final override uint read (void[] dst)
+        {
+                uint len = 0;
+
+                while (len < dst.length)
+                      {
+                      auto i = host.read (dst [len .. $]);
+                      if (i is IConduit.Eof)
+                          return (len ? len : i);
+                      len += i;
+                      } 
+                return len;
+        }
+
+        /***********************************************************************
+        
+                Read from a stream into a target array. The provided dst
+                will be fully populated with content from the input. 
+
+                This differs from read in that it will throw an exception
+                where an Eof condition is reached before input has completed
+
+        ***********************************************************************/
+
+        final GreedyInput readExact (void[] dst)
+        {
+                while (dst.length)
+                      {
+                      auto i = read (dst);
+                      if (i is Eof)
+                          conduit.error ("unexpected Eof while reading: "~conduit.toString);
+                      dst = dst [i .. $];
+                      }
+                return this;
+        }          
+}
+
+
+
 /*******************************************************************************
 
         A conduit filter that ensures its output is written in full  
@@ -55,47 +122,39 @@ class GreedyOutput : OutputFilter
                       } 
                 return len;
         }
+                             
+        /***********************************************************************
+        
+                Write to stream from a source array. The provided src content 
+                will be written in full to the output.
+
+                This differs from write in that it will throw an exception
+                where an Eof condition is reached before output has completed
+
+        ***********************************************************************/
+
+        final GreedyOutput writeExact (void[] src)
+        {
+                while (src.length)
+                      {
+                      auto i = write (src);
+                      if (i is Eof)
+                          conduit.error ("unexpected Eof while writing: "~conduit.toString);
+                      src = src [i .. $];
+                      }
+                return this;
+        }       
 }
 
 
 /*******************************************************************************
 
-        A conduit filter that ensures its input is read in full         
-
 *******************************************************************************/
 
-class GreedyInput : InputFilter
+debug (GreedyStream)
 {
-        /***********************************************************************
-
-                Propagate ctor to superclass
-
-        ***********************************************************************/
-
-        this (InputStream stream)
-        {
-                super (stream);
-        }
-
-        /***********************************************************************
-
-                Fill the provided array. Returns the number of bytes
-                actually read, which will be less that dst.length when
-                Eof has been reached, and then Eof thereafter
-
-        ***********************************************************************/
-
-        final override uint read (void[] dst)
-        {
-                uint len = 0;
-
-                while (len < dst.length)
-                      {
-                      auto i = host.read (dst [len .. $]);
-                      if (i is IConduit.Eof)
-                          return (len ? len : i);
-                      len += i;
-                      } 
-                return len;
+        void main()
+        {       
+                auto s = new GreedyInput (null);
         }
 }
