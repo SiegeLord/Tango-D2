7af889028d0c0b4bfa47e6d12f3e515b64fe4303
schveiguy <schveiguy@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2007-12-10 18:45:31 +0000

    fixed dsss.conf to use tango/time instead of tango/util/time updated comments in DefaultCalendar.d

--- tango/time/chrono/DefaultCalendar.d
+++ tango/time/chrono/DefaultCalendar.d
@@ -1,7 +1,5 @@
 /**
- * Default calendar.  Use this module to have a system-wide calendar.  By
- * default, the calendar instance is set to GregorianCalendar.  To change it,
- * simply assign it.
+ * Default calendar.  Use this module to have a system-wide calendar.
  */
 
 module tango.time.chrono.DefaultCalendar;
@@ -9,6 +7,10 @@ module tango.time.chrono.DefaultCalendar;
 private import tango.time.chrono.Calendar,
                tango.time.chrono.Gregorian;
 
+/**
+ * System-wide default calendar instance.  By default, the calendar instance
+ * is set to GregorianCalendar.  To change it, simply assign it.
+ */
 public static Calendar DefaultCalendar;
 
 static this()
