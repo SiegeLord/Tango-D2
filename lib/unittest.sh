4288112e2a1d426a05dee622ae4f525de1921be7
larsivi <larsivi@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2008-02-16 13:56:07 +0000

    Can now create a unittest runner that reports and continues instead of aborting.

--- lib/unittest.sh
+++ lib/unittest.sh
@@ -12,8 +12,22 @@ die() {
     exit $2
 }
 
+usage() {
+    echo 'Usage: ./unittest.sh [otions ...]
+Options:
+  --help: This message
+  --run-all: Reports result instead of breaking. Do not use this if you want to
+         run unittest runner through a debugger.
+  dmd: Builds unittests for dmd
+  gdc: Builds unittests for gdc
+
+  <none>: Builds unittests for all known compilers.'
+  exit 0
+}
+
 DC=
 LIB=
+RUNALL=
 
 compile() {
 
@@ -31,10 +45,44 @@ compile() {
         cat > $EXE.d <<EOF
 module ${EXE};
 
+import tango.io.Stdout;
+import tango.core.Runtime;
+
+bool tangoUnitTester()
+{
+    uint count = 0;
+    Stdout ("NOTE: This is still fairly rudimentary, and will only report the").newline;
+    Stdout ("    first error per module.").newline;
+    foreach ( m; ModuleInfo )  // _moduleinfo_array )
+    {
+        if ( m.unitTest) {
+            Stdout.format ("{}. Executing unittests in '{}' ", count, m.name);
+            try {
+               m.unitTest();
+            }
+            catch (Exception e) {
+                Stdout(" - Unittest failed.").newline;
+                Stdout.format("   File '{}', line '{}'.", e.file, e.line).newline;
+                Stdout.format("     Message is : '{}'", e.msg).newline;
+                if (e.info)
+                    Stdout.format("     TraceInfo: {}", e.info.toString).newline;
+                continue;
+            }
+            Stdout(" - Success.").newline;
+            count++;
+        }
+    }
+    return true;
+}
+
+static this() {
+    $RUNALL    
+}
+
 void main() {}
 EOF
 
-        rebuild -d -L-ldl -L-lz -debug=UnitTest -debug -full -clean -unittest -version=UnitTest $EXE.d tango/core/*.d tango/io/digest/*.d tango/io/model/*.d tango/io/protocol/*.d tango/io/selector/*.d tango/io/*.d tango/io/vfs/*.d tango/io/vfs/model/* tango/math/*.d tango/net/ftp/*.d tango/net/http/*.d tango/net/model/*.d tango/stdc/stringz.d tango/sys/*.d tango/text/convert/*.d tango/text/locale/Collation.d tango/text/locale/Convert.d tango/text/locale/Core.d tango/text/locale/Data.d tango/text/locale/Locale.d tango/text/locale/Parse.d tango/text/locale/Posix.d tango/text/stream/*.d tango/text/*.d tango/util/*.d tango/util/collection/model/*.d tango/util/collection/*.d tango/util/collection/iterator/*.d tango/util/collection/impl/*.d tango/util/log/model/*.d tango/util/log/*.d tango/time/chrono/*.d tango/time/*.d -dc=$DC-posix-tango
+        rebuild -w -d -g -L-ldl -L-lz -debug=UnitTest -debug -full -clean -unittest -version=UnitTest $EXE.d tango/core/*.d tango/io/digest/*.d tango/io/model/*.d tango/io/protocol/*.d tango/io/selector/*.d tango/io/*.d tango/io/vfs/*.d tango/io/vfs/model/* tango/math/*.d tango/net/ftp/*.d tango/net/http/*.d tango/net/model/*.d tango/stdc/stringz.d tango/sys/*.d tango/text/convert/*.d tango/text/locale/Collation.d tango/text/locale/Convert.d tango/text/locale/Core.d tango/text/locale/Data.d tango/text/locale/Locale.d tango/text/locale/Parse.d tango/text/locale/Posix.d tango/text/stream/*.d tango/text/*.d tango/util/*.d tango/util/collection/model/*.d tango/util/collection/*.d tango/util/collection/iterator/*.d tango/util/collection/impl/*.d tango/util/log/model/*.d tango/util/log/*.d tango/time/chrono/*.d tango/time/*.d -dc=$DC-posix-tango
 
         mv $EXE lib/$EXE
         rm $EXE.d
@@ -44,5 +92,40 @@ EOF
 
 }
 
-compile dmd runUnitTest_dmd
-compile gdc runUnitTest_gdc
+while [ "$#" != "0" ]
+do
+    case "$1" in
+        --help)
+            usage
+            ;;
+        --run-all)
+            RUNALL="Runtime.moduleUnitTester( &tangoUnitTester );"
+            ;;
+        dmd)
+            DMD=1
+            ;;
+        gdc)
+            GDC=1
+            ;;
+        *)
+            usage
+            ;;
+    esac
+    shift
+done
+
+if [ ! "$DMD" -a ! "$GDC" ]
+then
+    DMD=1
+    GDC=1
+fi
+
+if [ "$DMD" = "1" ]
+then
+    compile dmd runUnitTest_dmd
+fi
+if [ "$GDC" = "1" ]
+then
+    compile gdc runUnitTest_gdc
+fi
+
