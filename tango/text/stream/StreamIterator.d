ef0fa1d82a670f2b0f159e369105b2cf5511f3c9
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2008-03-21 06:12:09 +0000

    fixes #970 :: foreach (index, line, delim; stream)
    Thanks, Daniel919

--- tango/text/stream/StreamIterator.d
+++ tango/text/stream/StreamIterator.d
@@ -54,7 +54,8 @@ private import tango.io.model.IConduit;
 
 class StreamIterator(T) : InputStream, Buffered
 {
-        protected T[]           slice;
+        protected T[]           slice,
+                                delim;
         private IBuffer         input;
 
         /***********************************************************************
@@ -156,6 +157,28 @@ class StreamIterator(T) : InputStream, Buffered
                 return result;
         }
 
+        /**********************************************************************
+
+                Iterate over a set of tokens and delimiters, exposing a 
+                token count starting at zero
+
+        **********************************************************************/
+
+        int opApply (int delegate(inout int, inout T[], inout T[]) dg)
+        {
+                bool more;
+                int  result,
+                     tokens;
+
+                do {
+                   delim = null;
+                   more = consume;
+                   result = dg (tokens, slice, delim);
+                   ++tokens;
+                   } while (more && !result);
+                return result;
+        }
+
         /***********************************************************************
 
                 Locate the next token. Returns the token if found, null
@@ -179,9 +202,6 @@ class StreamIterator(T) : InputStream, Buffered
                 foreach (line; new LineIterator!(char) (new FileConduit("myfile")))
                          Cout(line).newline;
                 ---
-
-                Note that tokens exposed via push() are returned immediately
-                when available, taking priority over the input stream itself
                 
         ***********************************************************************/
 
@@ -194,13 +214,29 @@ class StreamIterator(T) : InputStream, Buffered
 
         /***********************************************************************
 
-                Set the content of the current slice
+                Set the content of the current slice to the provided 
+                start and end points
 
         ***********************************************************************/
 
         protected final uint set (T* content, uint start, uint end)
         {
                 slice = content [start .. end];
+                return end;
+        }
+
+        /***********************************************************************
+
+                Set the content of the current slice to the provided 
+                start and end points, and delimiter to the segment
+                between end & next (inclusive)
+
+        ***********************************************************************/
+
+        protected final uint set (T* content, uint start, uint end, uint next)
+        {
+                slice = content [start .. end];
+                delim = content [end .. next+1];
                 return end;
         }
 
