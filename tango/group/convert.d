8b76e9b40b9c71d462a019666d8524edb79ea0e9
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2008-03-30 20:29:30 +0000

    added pragma(msg) to track usage of tango.group

--- tango/group/convert.d
+++ tango/group/convert.d
@@ -14,6 +14,8 @@
 
 module tango.group.convert;
 
+pragma (msg, "Please post your usage of tango.group to this ticket: http://dsource.org/projects/tango/ticket/1013");
+
 public import Utf = tango.text.convert.Utf;
 public import Float = tango.text.convert.Float;
 public import Integer = tango.text.convert.Integer;
