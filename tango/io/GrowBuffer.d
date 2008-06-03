4a09262939f328456e00c7d26377ef090a7969c0
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2008-06-03 20:08:04 +0000

    formatting

--- tango/io/GrowBuffer.d
+++ tango/io/GrowBuffer.d
@@ -13,6 +13,7 @@
 module tango.io.GrowBuffer;
 
 public import tango.io.model.IBuffer;
+
 public import tango.io.Buffer : GrowBuffer;
 
 pragma (msg, "tango.io.GrowBuffer is being migrated - please import tango.io.Buffer instead");
