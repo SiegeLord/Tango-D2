ba8f70597c0dd78d1e51c6673988f1102141a9c4
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2008-06-03 20:09:30 +0000

    migrated FileRoots to FileSystem instead

--- tango/io/FileSystem.d
+++ tango/io/FileSystem.d
@@ -19,10 +19,19 @@ private import  tango.io.FilePath;
 
 private import  tango.core.Exception;
 
+version (Win32)
+        {
+        private import Text = tango.text.Util;
+        private extern (Windows) DWORD GetLogicalDriveStringsA (DWORD, LPTSTR);
+        }
+
 version (Posix)
         {
         private import tango.stdc.string;
         private import tango.stdc.posix.unistd;
+
+        private import tango.io.FileConduit;
+        private import Integer = tango.text.convert.Integer;
         }
 
 /*******************************************************************************
@@ -184,6 +193,31 @@ struct FileSystem
 
                         return path;
                 }
+
+                /***************************************************************
+                        
+                        List the set of root devices (C:, D: etc)
+
+                ***************************************************************/
+
+                static char[][] list ()
+                {
+                        int             len;
+                        char[]          str;
+                        char[][]        roots;
+
+                        // acquire drive strings
+                        len = GetLogicalDriveStringsA (0, null);
+                        if (len)
+                           {
+                           str = new char [len];
+                           GetLogicalDriveStringsA (len, str.ptr);
+
+                           // split roots into seperate strings
+                           roots = Text.delimit (str [0 .. $-1], "\0");
+                           }
+                        return roots;
+                }
         }
 
         /***********************************************************************
@@ -225,6 +259,55 @@ struct FileSystem
                         auto path = s[0 .. strlen(s)+1].dup;
                         path[$-1] = '/';
                         return path;
+                }
+
+                /***************************************************************
+
+                        List the set of root devices.
+
+                 ***************************************************************/
+
+                static char[][] list ()
+                {
+                        version(darwin)
+                        {
+                            assert(0);
+                        }
+                        else
+                        {
+                            char[] path = "";
+                            char[][] list;
+                            int spaces;
+
+                            auto fc = new FileConduit("/etc/mtab");
+                            scope (exit)
+                                   fc.close;
+                            
+                            auto content = new char[cast(int) fc.length];
+                            fc.input.read (content);
+                            
+                            for(int i = 0; i < content.length; i++)
+                            {
+                                if(content[i] == ' ') spaces++;
+                                else if(content[i] == '\n')
+                                {
+                                    spaces = 0;
+                                    list ~= path;
+                                    path = "";
+                                }
+                                else if(spaces == 1)
+                                {
+                                    if(content[i] == '\\')
+                                    {
+                                        path ~= Integer.parse(content[++i..i+3], 8u);
+                                        i += 2;
+                                    }
+                                    else path ~= content[i];
+                                }
+                            }
+                            
+                            return list;
+                        }
                 }
         }
 }
