49e06b0650ceedaa4511fdfdb0fe37f17e89ecf2
sean <sean@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2008-07-25 18:36:35 +0000

    Applied patch.  This closes #1092
    Please note however that we shouldn't really be using tango.sys.linux.socket any more anyway, since we now have tango.stdc.posix.sys.socket.  But it seemed prudent to close this ticket regardless.

--- tango/sys/linux/socket.d
+++ tango/sys/linux/socket.d
@@ -37,285 +37,366 @@ int getnameinfo(sockaddr* sa, socklen_t salen, char* node, socklen_t nodelen, ch
 
 enum: int
 {
-	AF_UNSPEC =     0,
-	AF_UNIX =       1,
-	AF_INET =       2,
-	AF_IPX =        4,
-	AF_APPLETALK =  5,
-	AF_INET6 =      10,
-	// ...
+    AF_UNSPEC =     0,
+    AF_UNIX =       1,
+    AF_INET =       2,
+    AF_IPX =        4,
+    AF_APPLETALK =  5,
+    AF_INET6 =      10,
+    // ...
 
-	PF_UNSPEC =     AF_UNSPEC,
-	PF_UNIX =       AF_UNIX,
-	PF_INET =       AF_INET,
-	PF_IPX =        AF_IPX,
-	PF_APPLETALK =  AF_APPLETALK,
-	PF_INET6 =      AF_INET6,
+    PF_UNSPEC =     AF_UNSPEC,
+    PF_UNIX =       AF_UNIX,
+    PF_INET =       AF_INET,
+    PF_IPX =        AF_IPX,
+    PF_APPLETALK =  AF_APPLETALK,
+    PF_INET6 =      AF_INET6,
 }
 
 
 version( X86 )
 {
-	enum: int
-	{
-		SOL_SOCKET =  1,
-	}
+    enum: int
+    {
+        SOL_SOCKET =  1,
+    }
 }
 else version( X86_64 )
 {
-	enum: int
-	{
-		SOL_SOCKET =  1,
-	}
+    enum: int
+    {
+        SOL_SOCKET =  1,
+    }
 }
 else version( SPARC )
 {
-	enum: int
-	{
-		SOL_SOCKET = 0xffff,
-	}
-}else
+    enum: int
+    {
+        SOL_SOCKET = 0xffff,
+    }
+}
+else version( SPARC64 )
 {
-	// Different values on other platforms.
-	static assert(0);
+    enum: int
+    {
+        SOL_SOCKET = 0xffff,
+    }
+}
+else version( MIPS )
+{
+    enum: int
+    {
+        SOL_SOCKET = 0xffff,
+    }
+}
+else version( PPC )
+{
+    enum: int
+    {
+        SOL_SOCKET = 1,
+    }
+}
+else version( PPC64 )
+{
+    enum: int
+    {
+        SOL_SOCKET = 1,
+    }
+}
+else version( ARM )
+{
+    enum: int
+    {
+        SOL_SOCKET = 1,
+    }
+}
+else
+{
+    // Different values on other platforms.
+    static assert(0);
 }
 
 
 enum: int
 {
-	SO_DEBUG =       1,
-	SO_BROADCAST =   6,
-	SO_REUSEADDR =   2,
-	SO_LINGER =      13,
-	SO_DONTLINGER =  ~SO_LINGER,
-	SO_OOBINLINE =   10,
-	SO_SNDBUF =      7,
-	SO_RCVBUF =      8,
-	SO_ACCEPTCONN =  30,
-	SO_DONTROUTE =   5,
-	SO_TYPE =        3,
+    version( SPARC )
+    {
+        SO_DEBUG =       0x0001,
+        SO_BROADCAST =   0x0020,
+        SO_REUSEADDR =   0x0004,
+        SO_LINGER =      0x0080,
+        SO_DONTLINGER =  ~SO_LINGER,
+        SO_OOBINLINE =   0x0100,
+        SO_SNDBUF =      0x1001,
+        SO_RCVBUF =      0x1002,
+        SO_ACCEPTCONN =  0x8000,
+        SO_DONTROUTE =   0x0010,
+        SO_TYPE =        0x1008,
+    }
+    else version( SPARC64 )
+    {
+        SO_DEBUG =       0x0001,
+        SO_BROADCAST =   0x0020,
+        SO_REUSEADDR =   0x0004,
+        SO_LINGER =      0x0080,
+        SO_DONTLINGER =  ~SO_LINGER,
+        SO_OOBINLINE =   0x0100,
+        SO_SNDBUF =      0x1001,
+        SO_RCVBUF =      0x1002,
+        SO_ACCEPTCONN =  0x8000,
+        SO_DONTROUTE =   0x0010,
+        SO_TYPE =        0x1008,
+    }
+    else version( MIPS )
+    {
+        SO_DEBUG =       0x0001,
+        SO_BROADCAST =   0x0020,
+        SO_REUSEADDR =   0x0004,
+        SO_LINGER =      0x0080,
+        SO_DONTLINGER =  ~SO_LINGER,
+        SO_OOBINLINE =   0x0100,
+        SO_SNDBUF =      0x1001,
+        SO_RCVBUF =      0x1002,
+        SO_ACCEPTCONN =  0x1009,
+        SO_DONTROUTE =   0x0010,
+        SO_TYPE =        0x1008,
+    }
+    else //x86, X86_64, PPC, PPC64, ARM...
+    {
+        SO_DEBUG =       1,
+        SO_BROADCAST =   6,
+        SO_REUSEADDR =   2,
+        SO_LINGER =      13,
+        SO_DONTLINGER =  ~SO_LINGER,
+        SO_OOBINLINE =   10,
+        SO_SNDBUF =      7,
+        SO_RCVBUF =      8,
+        SO_ACCEPTCONN =  30,
+        SO_DONTROUTE =   5,
+        SO_TYPE =        3,
+    }
 
-	TCP_NODELAY =    1,
+    TCP_NODELAY =    1,
 
-	IP_MULTICAST_LOOP =  34,
-	IP_ADD_MEMBERSHIP =  35,
-	IP_DROP_MEMBERSHIP = 36,
+    IP_MULTICAST_LOOP =  34,
+    IP_ADD_MEMBERSHIP =  35,
+    IP_DROP_MEMBERSHIP = 36,
 
-	// ...
+    // ...
 
-	IPV6_ADDRFORM =        1,
-	IPV6_PKTINFO =         2,
-	IPV6_HOPOPTS =         3,
-	IPV6_DSTOPTS =         4,
-	IPV6_RTHDR =           5,
-	IPV6_PKTOPTIONS =      6,
-	IPV6_CHECKSUM =        7,
-	IPV6_HOPLIMIT =        8,
-	IPV6_NEXTHOP =         9,
-	IPV6_AUTHHDR =         10,
-	IPV6_UNICAST_HOPS =    16,
-	IPV6_MULTICAST_IF =    17,
-	IPV6_MULTICAST_HOPS =  18,
-	IPV6_MULTICAST_LOOP =  19,
-	IPV6_JOIN_GROUP =      20,
-	IPV6_LEAVE_GROUP =     21,
-	IPV6_ROUTER_ALERT =    22,
-	IPV6_MTU_DISCOVER =    23,
-	IPV6_MTU =             24,
-	IPV6_RECVERR =         25,
-	IPV6_V6ONLY =          26,
-	IPV6_JOIN_ANYCAST =    27,
-	IPV6_LEAVE_ANYCAST =   28,
-	IPV6_IPSEC_POLICY =    34,
-	IPV6_XFRM_POLICY =     35,
+    IPV6_ADDRFORM =        1,
+    IPV6_PKTINFO =         2,
+    IPV6_HOPOPTS =         3,
+    IPV6_DSTOPTS =         4,
+    IPV6_RTHDR =           5,
+    IPV6_PKTOPTIONS =      6,
+    IPV6_CHECKSUM =        7,
+    IPV6_HOPLIMIT =        8,
+    IPV6_NEXTHOP =         9,
+    IPV6_AUTHHDR =         10,
+    IPV6_UNICAST_HOPS =    16,
+    IPV6_MULTICAST_IF =    17,
+    IPV6_MULTICAST_HOPS =  18,
+    IPV6_MULTICAST_LOOP =  19,
+    IPV6_JOIN_GROUP =      20,
+    IPV6_LEAVE_GROUP =     21,
+    IPV6_ROUTER_ALERT =    22,
+    IPV6_MTU_DISCOVER =    23,
+    IPV6_MTU =             24,
+    IPV6_RECVERR =         25,
+    IPV6_V6ONLY =          26,
+    IPV6_JOIN_ANYCAST =    27,
+    IPV6_LEAVE_ANYCAST =   28,
+    IPV6_IPSEC_POLICY =    34,
+    IPV6_XFRM_POLICY =     35,
 }
 
 
 struct linger
 {
-	int32_t l_onoff;
-	int32_t l_linger;
+    int32_t l_onoff;
+    int32_t l_linger;
 }
 
 
 struct protoent
 {
-	char* p_name;
-	char** p_aliases;
-	int32_t p_proto;
+    char* p_name;
+    char** p_aliases;
+    int32_t p_proto;
 }
 
 
 struct servent
 {
-	char* s_name;
-	char** s_aliases;
-	int32_t s_port;
-	char* s_proto;
+    char* s_name;
+    char** s_aliases;
+    int32_t s_port;
+    char* s_proto;
 }
 
 
 version( BigEndian )
 {
-	uint16_t htons(uint16_t x)
-	{
-		return x;
-	}
+    uint16_t htons(uint16_t x)
+    {
+        return x;
+    }
 
 
-	uint32_t htonl(uint32_t x)
-	{
-		return x;
-	}
+    uint32_t htonl(uint32_t x)
+    {
+        return x;
+    }
 }
 else version( LittleEndian )
 {
-	private import tango.core.BitManip;
+    private import tango.core.BitManip;
 
 
-	uint16_t htons(uint16_t x)
-	{
-		return cast(uint16_t) ((x >> 8) | (x << 8));
-	}
+    uint16_t htons(uint16_t x)
+    {
+        return cast(uint16_t) ((x >> 8) | (x << 8));
+    }
 
 
-	uint32_t htonl(uint32_t x)
-	{
-		return bswap(x);
-	}
+    uint32_t htonl(uint32_t x)
+    {
+        return bswap(x);
+    }
 }
 else
 {
-	static assert(0);
+    static assert(0);
 }
 
 
 uint16_t ntohs(uint16_t x)
 {
-	return htons(x);
+    return htons(x);
 }
 
 
 uint32_t ntohl(uint32_t x)
 {
-	return htonl(x);
+    return htonl(x);
 }
 
 
 enum: int
 {
-	SOCK_STREAM =     1,
-	SOCK_DGRAM =      2,
-	SOCK_RAW =        3,
-	SOCK_RDM =        4,
-	SOCK_SEQPACKET =  5,
+    SOCK_STREAM =     1,
+    SOCK_DGRAM =      2,
+    SOCK_RAW =        3,
+    SOCK_RDM =        4,
+    SOCK_SEQPACKET =  5,
 }
 
 
 enum: int
 {
-	IPPROTO_IP =    0,
-	IPPROTO_ICMP =  1,
-	IPPROTO_IGMP =  2,
-	IPPROTO_GGP =   3,
-	IPPROTO_TCP =   6,
-	IPPROTO_PUP =   12,
-	IPPROTO_UDP =   17,
-	IPPROTO_IDP =   22,
-	IPPROTO_IPV6 =  41,
-	IPPROTO_ND =    77,
-	IPPROTO_RAW =   255,
+    IPPROTO_IP =    0,
+    IPPROTO_ICMP =  1,
+    IPPROTO_IGMP =  2,
+    IPPROTO_GGP =   3,
+    IPPROTO_TCP =   6,
+    IPPROTO_PUP =   12,
+    IPPROTO_UDP =   17,
+    IPPROTO_IDP =   22,
+    IPPROTO_IPV6 =  41,
+    IPPROTO_ND =    77,
+    IPPROTO_RAW =   255,
 
-	IPPROTO_MAX =   256,
+    IPPROTO_MAX =   256,
 }
 
 
 enum: int
 {
-	MSG_OOB =        0x1,
-	MSG_PEEK =       0x2,
-	MSG_DONTROUTE =  0x4,
+    MSG_OOB =        0x1,
+    MSG_PEEK =       0x2,
+    MSG_DONTROUTE =  0x4,
 }
 
 
 enum: int
 {
-	SD_RECEIVE =  0,
-	SD_SEND =     1,
-	SD_BOTH =     2,
+    SD_RECEIVE =  0,
+    SD_SEND =     1,
+    SD_BOTH =     2,
 }
 
 
 enum: uint
 {
-	INADDR_ANY =        0,
-	INADDR_LOOPBACK =   0x7F000001,
-	INADDR_BROADCAST =  0xFFFFFFFF,
-	INADDR_NONE =       0xFFFFFFFF,
-	ADDR_ANY =          INADDR_ANY,
+    INADDR_ANY =        0,
+    INADDR_LOOPBACK =   0x7F000001,
+    INADDR_BROADCAST =  0xFFFFFFFF,
+    INADDR_NONE =       0xFFFFFFFF,
+    ADDR_ANY =          INADDR_ANY,
 }
 
 
 enum: int
 {
-	AI_PASSIVE = 0x1,
-	AI_CANONNAME = 0x2,
-	AI_NUMERICHOST = 0x4,
+    AI_PASSIVE = 0x1,
+    AI_CANONNAME = 0x2,
+    AI_NUMERICHOST = 0x4,
 }
 
 
 union in_addr
 {
-	private union _S_un_t
-	{
-		private struct _S_un_b_t
-		{
-			uint8_t s_b1, s_b2, s_b3, s_b4;
-		}
-		_S_un_b_t S_un_b;
+    private union _S_un_t
+    {
+        private struct _S_un_b_t
+        {
+            uint8_t s_b1, s_b2, s_b3, s_b4;
+        }
+        _S_un_b_t S_un_b;
 
-		private struct _S_un_w_t
-		{
-			uint16_t s_w1, s_w2;
-		}
-		_S_un_w_t S_un_w;
+        private struct _S_un_w_t
+        {
+            uint16_t s_w1, s_w2;
+        }
+        _S_un_w_t S_un_w;
 
-		uint32_t S_addr;
-	}
-	_S_un_t S_un;
+        uint32_t S_addr;
+    }
+    _S_un_t S_un;
 
-	uint32_t s_addr;
+    uint32_t s_addr;
 
-	struct
-	{
-		uint8_t s_net, s_host;
+    struct
+    {
+        uint8_t s_net, s_host;
 
-		union
-		{
-			uint16_t s_imp;
+        union
+        {
+            uint16_t s_imp;
 
-			struct
-			{
-				uint8_t s_lh, s_impno;
-			}
-		}
-	}
+            struct
+            {
+                uint8_t s_lh, s_impno;
+            }
+        }
+    }
 }
 
 
 union in6_addr
 {
-	private union _in6_u_t
-	{
-		uint8_t[16] u6_addr8;
-		uint16_t[8] u6_addr16;
-		uint32_t[4] u6_addr32;
-	}
-	_in6_u_t in6_u;
+    private union _in6_u_t
+    {
+        uint8_t[16] u6_addr8;
+        uint16_t[8] u6_addr16;
+        uint32_t[4] u6_addr32;
+    }
+    _in6_u_t in6_u;
 
-	uint8_t[16] s6_addr8;
-	uint16_t[8] s6_addr16;
-	uint32_t[4] s6_addr32;
+    uint8_t[16] s6_addr8;
+    uint16_t[8] s6_addr16;
+    uint32_t[4] s6_addr32;
 }
 
 
@@ -330,55 +411,55 @@ const uint INET6_ADDRSTRLEN = 46;
 
 struct sockaddr
 {
-	int16_t sa_family;
-	ubyte[14] sa_data;
+    int16_t sa_family;
+    ubyte[14] sa_data;
 }
 
 
 struct sockaddr_in
 {
-	int16_t sin_family = AF_INET;
-	uint16_t sin_port;
-	in_addr sin_addr;
-	ubyte[8] sin_zero;
+    int16_t sin_family = AF_INET;
+    uint16_t sin_port;
+    in_addr sin_addr;
+    ubyte[8] sin_zero;
 }
 
 
 struct sockaddr_in6
 {
-	int16_t sin6_family = AF_INET6;
-	uint16_t sin6_port;
-	uint32_t sin6_flowinfo;
-	in6_addr sin6_addr;
-	uint32_t sin6_scope_id;
+    int16_t sin6_family = AF_INET6;
+    uint16_t sin6_port;
+    uint32_t sin6_flowinfo;
+    in6_addr sin6_addr;
+    uint32_t sin6_scope_id;
 }
 
 
 struct addrinfo
 {
-	int32_t ai_flags;
-	int32_t ai_family;
-	int32_t ai_socktype;
-	int32_t ai_protocol;
-	size_t ai_addrlen;
-	sockaddr* ai_addr;
-	char* ai_canonname;
-	addrinfo* ai_next;
+    int32_t ai_flags;
+    int32_t ai_family;
+    int32_t ai_socktype;
+    int32_t ai_protocol;
+    size_t ai_addrlen;
+    sockaddr* ai_addr;
+    char* ai_canonname;
+    addrinfo* ai_next;
 }
 
 
 struct hostent
 {
-	char* h_name;
-	char** h_aliases;
-	int32_t h_addrtype;
-	int32_t h_length;
-	char** h_addr_list;
+    char* h_name;
+    char** h_aliases;
+    int32_t h_addrtype;
+    int32_t h_length;
+    char** h_addr_list;
 
 
-	char* h_addr()
-	{
-		return h_addr_list[0];
-	}
+    char* h_addr()
+    {
+        return h_addr_list[0];
+    }
 }
 
