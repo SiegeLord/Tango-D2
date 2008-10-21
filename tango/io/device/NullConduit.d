3a2ad242317879eb93359dceab65c503fe0b59cc
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2008-10-21 05:38:03 +0000

    added a comment about the return value of write()

--- tango/io/device/NullConduit.d
+++ tango/io/device/NullConduit.d
@@ -19,15 +19,17 @@ private import tango.io.device.Conduit;
 /*******************************************************************************
 
         A Conduit that ignores all that is written to it and returns Eof
-        when read from
+        when read from. Note that write() returns the length of what was
+        handed to it, acting as a pure bit-bucket. Returning zero or Eof
+        instead would not be appropriate in this context.
 
 *******************************************************************************/
 
 class NullConduit : Conduit
 {
-        override char[] toString () {return "NullConduit";} 
+        override char[] toString () {return "<null conduit>";} 
 
-        override uint bufferSize () { return 256u;}
+        override uint bufferSize () { return 0;}
 
         override uint read (void[] dst) { return Eof; }
 
