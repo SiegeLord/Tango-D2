44da609c08dad0704bd9937e6099476f15d9abf3
fawzi <fawzi@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2009-03-04 22:13:21 +0000

    adding constants.autoconf (renaming autoConf to autoconf on a case insensitive fs was impossible)

---
+++ tango/stdc/constants/autoconf/errno.d
@@ -0,0 +1,6 @@
+module tango.stdc.constants.autoconf.errno;
+version(autoconf){
+    pragma(msg,"non generated constants you can try to generate new ones running tango/lib/constants/dppAll.sh or dppAll2.sh and manual editing and then use -version=autoconf");
+    pragma(msg,"please contact the tango team and help porting tango to your platform");
+    static assert(0,"non generated constants in "~__FILE__);
+}
