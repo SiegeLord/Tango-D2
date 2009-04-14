b43b2bf035039fb83f0db379e3743d25e0d2f2cd
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2009-04-14 01:12:41 +0000

    fixes #1591 :: bug in HttpGet.read()
    thanks to tg

--- tango/net/http/HttpGet.d
+++ tango/net/http/HttpGet.d
@@ -70,7 +70,7 @@ class HttpGet : HttpClient
                 auto buffer = super.open;
                 try {
                     if (super.isResponseOK)
-                        buffer.load (getResponseHeaders.getInt(HttpHeader.ContentLength, uint.max));
+                        buffer.load (getResponseHeaders.getInt(HttpHeader.ContentLength, int.max));
                     } finally {super.close;}
                 return buffer.slice;
         }
