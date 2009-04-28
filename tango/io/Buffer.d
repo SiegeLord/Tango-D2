a105eabc93c4e5fd83e7b9e6cbb59fbe95e6eb3c
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2009-04-28 02:17:41 +0000

    clarified pragma message

--- tango/io/Buffer.d
+++ tango/io/Buffer.d
@@ -18,7 +18,7 @@ private import  tango.core.Exception;
 public  import  tango.io.model.IBuffer,
                 tango.io.model.IConduit;
 
-pragma (msg, "revision: io.Buffer functionality has been split into io.stream.Buffered and io.device.Array - use the former for streaming, and the latter for virtual/memory IO");
+pragma (msg, "revision: io.Buffer functionality has been split into io.stream.Buffered and io.device.Array - use the former for streaming, and the latter for memory IO");
 
 
 /******************************************************************************
