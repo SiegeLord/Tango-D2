7ed0a2624364af25885be10ce20316ddac604587
fawzi <fawzi@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2009-05-09 21:26:26 +0000

    better flags for gcc

--- lib/constants/dpp2.sh
+++ lib/constants/dpp2.sh
@@ -2,8 +2,9 @@
 #preprocess DD files
 CPP=`which gcc`
 if [ -e $CPP ]
-    then CPP="gcc -E"
+    then CPP="gcc -E -dD -P -C "
 else
     CPP=`which cpp`
 fi
-$CPP -P -C $* | sed -e '/^# [0-9]{1,} / d' -e 's/__XYX__//g'
+
+$CPP $* | sed -e '/^# [0-9]{1,} / d' -e 's/__XYX__//g'
