75310639a84e76b3286f28591c26cd3c1504ad4d
fawzi <fawzi@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2009-06-22 09:25:27 +0000

    made grow function public, improved rule files (output name, lib flags, leave DFLAGS_ADD as external addition)

--- lib/common/tango/core/Memory.d
+++ lib/common/tango/core/Memory.d
@@ -440,4 +440,66 @@ struct GC
     {
         gc_removeRange( p );
     }
+    
+    /**
+    * returns the amount to allocate to keep some extra space
+    * for large allocations the extra allocated space decreases, but is still enough
+    * so that the number of reallocations when linearly growing stays logaritmic
+    * Params:
+    * newlength = the number of elements to allocate
+    * elSize = size of one element
+    */
+    size_t growLength(size_t newlength,size_t elSize=1){
+        size_t newcap = newlength*elSize;
+        size_t newext = 0;
+        const size_t b=0; // flatness factor, how fast the extra space decreases with array size
+        const size_t a=100; // allocate at most a% of the requested size as extra space (rounding will change this)
+        const size_t minBits=1; // minimum bit size
+    
+
+        static size_t log2plusB(size_t c)
+        {
+            // could use the bsr bit op
+            size_t i=b+1;
+            while(c >>= 1){
+                ++i;
+            }
+            return i;
+        }
+        long mult = 100 + a*(minBits+b) / log2plusB(newlength);
+
+        newext = elSize*cast(size_t)(((newcap * mult)+99) / 100);
+        newcap = newext > newcap ? newext : newcap; // just to handle overflows
+        return newcap;
+    }
+    
+    /**
+    * returns the amount to allocate to keep some extra space
+    * for large allocations the extra allocated space decreases, but is still enough
+    * so that the number of reallocations when linearly growing stays logaritmic
+    * Params:
+    * newlength = the number of elements to allocate
+    * elSize = size of one element
+    * a = maximum extra space in percent (the allocated space gets rounded up, so might be larger)
+    * b = flatness factor, how fast the extra space decreases with array size (the larger the more constant)
+    * minBits = minimum number of bits of newlength
+    */
+    size_t growLength(size_t newlength, size_t elSize,size_t a, size_t b=0,size_t minBits=1){
+        size_t newcap = newlength*elSize;
+        size_t newext = 0;
+        static size_t log2(size_t c)
+        {
+            // could use the bsr bit op
+            size_t i=1;
+            while(c >>= 1){
+                ++i;
+            }
+            return i;
+        }
+        long mult = 100 + a*(minBits+b) / (log2(newlength)+b);
+
+        newext = elSize*cast(size_t)(((newcap * mult)+99) / 100);
+        newcap = newext > newcap ? newext : newcap; // just to handle overflows
+        return newcap;
+    }
 }
