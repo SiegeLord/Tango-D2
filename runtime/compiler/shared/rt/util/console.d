fd5915f3f01e0246d22cf93a145d955770da1874
fawzi <fawzi@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2009-08-15 19:53:51 +0000

    remove dependecies on stdc, use stdio.

--- runtime/compiler/shared/rt/util/console.d
+++ runtime/compiler/shared/rt/util/console.d
@@ -15,36 +15,22 @@ module rt.util.console;
 
 private import rt.util.string;
 
-version (Win32) {
-        private extern (Windows) int GetStdHandle (int);
-        private extern (Windows) int WriteFile (int, char*, int, int*, void*);
-} else version (Posix){
-    import tango.stdc.posix.unistd;
-}
+import tango.stdc.stdio;
 
 struct Console
 {
     Console opCall (char[] s)
     {
-            version (Win32)
-                    {
-                    int count;
-                    WriteFile (GetStdHandle(0xfffffff5), s.ptr, s.length, &count, null);
-                    }
-            else
-            version (Posix)
-                    {
-                    write (2, s.ptr, s.length);
-                    }
-            return *this;
+        fprintf(stderr, "%.*s",s.length,s.ptr);
+        return *this;
     }
 
     // emit an integer to the console
     Console opCall (size_t i)
     {
-            char[25] tmp = void;
+        char[25] tmp = void;
 
-            return console (ulongToUtf8 (tmp,cast(ulong) i));
+        return console (ulongToUtf8 (tmp,cast(ulong) i));
     }
 }
 
