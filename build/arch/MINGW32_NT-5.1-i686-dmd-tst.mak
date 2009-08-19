86a3709bda76b0717eff9d770277c4bcfec6fe7d
h3r3tic <h3r3tic@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2009-08-19 01:21:41 +0000

    fixed an invalid assertion in rt/basicgc/gcx that caused programs to go awry when running with the debug runtime; removed -w from tst and dbg builds (temporarily?) since they would not build

--- build/arch/MINGW32_NT-5.1-i686-dmd-tst.mak
+++ build/arch/MINGW32_NT-5.1-i686-dmd-tst.mak
@@ -1,5 +1,5 @@
 include $(ARCHDIR)/dmd-win.rules
 include $(ARCHDIR)/mingw.inc
 
-DFLAGS_COMP=-g -w -d -unittest -debug=UnitTest
+DFLAGS_COMP=-g -d -unittest -debug=UnitTest
 CFLAGS_COMP=-g -mn -6 -r
--- runtime/gc/basic/rt/basicgc/gcx.d
+++ runtime/gc/basic/rt/basicgc/gcx.d
@@ -2581,7 +2581,7 @@ struct Gcx
         debug(COLLECT_PRINTF) printf("recovered pages = %d\n", recoveredpages);
         debug(COLLECT_PRINTF) printf("\tfree'd %u bytes, %u pages from %u pools\n", freed, freedpages, npools);
         auto oldV=flagAdd!(size_t)(gcCounter,1);
-        assert(oldV&cast(size_t)1 == 0,"unexpected gc counter value");
+        assert((oldV&cast(size_t)1) == 1,"unexpected gc counter value");
 
         return freedpages + recoveredpages;
     }
