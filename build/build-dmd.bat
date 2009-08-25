b37a192492a0b371fdb3d996dabdf062ddfbc628
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2009-08-25 05:08:08 +0000

    reverted installation additions

--- build/build-dmd.bat
+++ build/build-dmd.bat
@@ -2,6 +2,6 @@
 set TANGO_OLDHOME=%HOME%
 set HOME=%CD%
 make clean-all -fdmd-win32.mak
-make all install -fdmd-win32.mak
+make all -fdmd-win32.mak
 make clean -fdmd-win32.mak
 set HOME=%TANGO_OLDHOME%
