545eda7ec28811908776b092278c741113980bfb
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2009-09-20 23:38:52 +0000

    delete old lib/

--- lib/constants/dpp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#preprocess DD files
-CPP=`which gcc`
-if [ -e $CPP ]
-    then CPP="gcc -E"
-else
-    CPP=`which cpp`
-fi
-$CPP -P -C $* | sed -e '1,/xx*  *start  *xx*/ d' -e '/^# [0-9]{1,} / d' -e 's/__XYX__//g'
