9cc8df4a9820b62cb9c737ad430bd8b5d33b0285
larsivi <larsivi@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2009-10-10 11:25:05 +0000

    Keep typeinfos separate, there is no guarantee that they can be kept shared.

---
+++ tango/core/rt/compiler/ldc/typeinfo/ti_ifloat.d
@@ -0,0 +1,11 @@
+
+// ifloat
+
+module rt.compiler.ldc.typeinfo.ti_ifloat;
+
+private import rt.compiler.ldc.typeinfo.ti_float;
+
+class TypeInfo_o : TypeInfo_f
+{
+    override char[] toString() { return "ifloat"; }
+}
---
+++ tango/core/rt/compiler/ldc/typeinfo/ti_ireal.d
@@ -0,0 +1,11 @@
+
+// ireal
+
+module rt.compiler.ldc.typeinfo.ti_ireal;
+
+private import rt.compiler.ldc.typeinfo.ti_real;
+
+class TypeInfo_j : TypeInfo_e
+{
+    override char[] toString() { return "ireal"; }
+}
