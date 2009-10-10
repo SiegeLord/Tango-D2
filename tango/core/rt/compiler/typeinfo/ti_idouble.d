cbc1230aa9af91816f476c7ffab6c10668480c66
larsivi <larsivi@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2009-10-10 05:40:34 +0000

    Typeinfos for ldc and dmd are shared

---
+++ tango/core/rt/compiler/typeinfo/ti_idouble.d
@@ -0,0 +1,11 @@
+
+// idouble
+
+module rt.compiler.typeinfo.ti_idouble;
+
+private import rt.compiler.typeinfo.ti_double;
+
+class TypeInfo_p : TypeInfo_d
+{
+    override char[] toString() { return "idouble"; }
+}
