adf89dae161709b661b27790aca530eb303462c9
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2009-11-13 20:22:01 +0000

    fixes #1775 :: Issue of Process.toNullEndedBuffer
    thanks to SHOO

--- tango/sys/Process.d
+++ tango/sys/Process.d
@@ -1850,7 +1850,7 @@ class Process
                 dest ~= key ~ '=' ~ value ~ '\0';
             }
 
-            dest ~= '\0';
+            dest ~= "\0\0";
             return dest;
         }
     }
