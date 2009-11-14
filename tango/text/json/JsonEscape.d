1dd91c66b1aa98d584d3e4261bcc15ef79781836
kris <kris@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2009-11-14 03:43:53 +0000

    fixes #1728 :: JSON shall escape \r and \n in strings.
    Thanks to keinfarbton

--- tango/text/json/JsonEscape.d
+++ tango/text/json/JsonEscape.d
@@ -32,114 +32,45 @@ private import Utf = tango.text.convert.Utf;
 
 T[] unescape(T) (T[] src, T[] dst = null)
 {
-        int delta;
-        auto s = src.ptr;
-        auto len = src.length;
-        enum:T {slash = '\\'};
+        size_t content;
 
-        // take a peek first to see if there's anything
-        if ((delta = Util.indexOf (s, slash, len)) < len)
-           {
-           // make some room if not enough provided
-           if (dst.length < src.length)
-               dst.length = src.length;
-           auto d = dst.ptr;
+        void append (T[] s)
+        {
+                if (content + s.length > dst.length)
+                    dst.length = dst.length + s.length + 1024;
+                dst[content .. content+s.length] = s;
+                content += s.length;
+        }
 
-           // copy segments over, a chunk at a time
-           do {
-              d [0 .. delta] = s [0 .. delta];
-              len -= delta;
-              s += delta;
-              d += delta;
+        unescape (src, &append);
+        return dst [0 .. content];
+}
 
-              // bogus trailing '\'
-              if (len < 2)
-                 {
-                 *d++ = '\\';
-                 len = 0;
-                 break;
-                 }
 
-              // translate \c
-              switch (s[1])
-                     {
-                      case '\\':
-                           *d++ = '\\';
-                           break;
+/******************************************************************************
 
-                      case '/':
-                           *d++ = '/';
-                           break;
+        Convert reserved chars to escaped ones. For example: \ => \\ 
 
-                      case '"':
-                           *d++ = '"';
-                           break;
+        Either a slice of the provided output buffer is returned, or the 
+        original content, depending on whether there were reserved chars
+        present or not. The output buffer will be expanded as necessary
+        
+******************************************************************************/
 
-                      case 'b':
-                           *d++ = '\b';
-                           break;
+T[] escape(T) (T[] src, T[] dst = null)
+{
+        size_t content;
 
-                      case 'f':
-                           *d++ = '\f';
-                           break;
+        void append (T[] s)
+        {
+                if (content + s.length > dst.length)
+                    dst.length = dst.length + s.length + 1024;
+                dst[content .. content+s.length] = s;
+                content += s.length;
+        }
 
-                      case 'n':
-                           *d++ = '\n';
-                           break;
-
-                      case 'r':
-                           *d++ = '\r';
-                           break;
-
-                      case 't':
-                           *d++ = '\t';
-                           break;
-
-                      case 'u':
-                           if (len < 6)
-                               goto default;
-                           else
-                              {
-                              dchar v = 0;
-                              T[6]  t = void;
-
-                              for (auto i=2; i < 6; ++i)
-                                  {
-                                  auto c = s[i];
-                                  if (c >= '0' && c <= '9')
-                                     {}
-                                  else
-                                     if (c >= 'a' && c <= 'f')
-                                         c -= 39;
-                                     else
-                                        if (c >= 'A' && c <= 'F')
-                                            c -= 7;
-                                        else
-                                           goto default;
-                                  v = (v << 4) + c - '0';
-                                  }
-                              
-                              auto c = Utf.fromString32 ((&v)[0..1], t);
-                              d [0 .. c.length] = c;
-                              d += c.length;
-                              len -= 4;
-                              s += 4;
-                              }
-                           break;
-
-                      default:
-                           throw new Exception ("invalid escape");
-                     }
-
-              s += 2;
-              len -= 2;           
-              } while ((delta = Util.indexOf (s, slash, len)) < len);
-
-           // copy tail too
-           d [0 .. len] = s [0 .. len];
-           return dst [0 .. (d + len) - dst.ptr];
-           }
-        return src;
+        escape (src, &append);
+        return dst [0..content];
 }
 
 
@@ -261,55 +192,6 @@ void unescape(T) (T[] src, void delegate(T[]) emit)
 
         Convert reserved chars to escaped ones. For example: \ => \\ 
 
-        Either a slice of the provided output buffer is returned, or the 
-        original content, depending on whether there were reserved chars
-        present or not. The output buffer should be at least twice the 
-        length of the provided src, or it will be allocated from the heap 
-        instead 
-        
-******************************************************************************/
-
-T[] escape(T) (T[] src, T[] dst = null)
-{
-        auto s = src.ptr;
-        auto t = s;
-        auto e = s + src.length;
-
-        // make some room if not enough provided
-        if (dst.length < src.length * 2)
-            dst.length = src.length * 2;
-        auto d = dst.ptr;
-
-        while (s < e)
-              {
-              if (*s is '"' || *s is '/' || *s is '\\')
-                 {
-                 auto len = s - t;
-                 d [0 .. len] = t [0 .. len];
-                 d += len;
-                 *d++ = '\\';
-                 t = s;
-                 }
-              ++s;           
-              }
-
-        // did we change anything?
-        if (d > dst.ptr)
-           {
-           // copy tail too
-           auto len = e - t;
-           d [0 .. len] = t [0 .. len];
-           return dst [0 .. d  + len - dst.ptr];
-           }
-
-        return src;
-}
-
-
-/******************************************************************************
-
-        Convert reserved chars to escaped ones. For example: \ => \\ 
-
         This variant does not require an interim workspace, and instead
         emits directly via the provided delegate
         
@@ -317,28 +199,49 @@ T[] escape(T) (T[] src, T[] dst = null)
 
 void escape(T) (T[] src, void delegate(T[]) emit)
 {
+        T[2] patch = '\\';
         auto s = src.ptr;
         auto t = s;
         auto e = s + src.length;
-        auto escaped = false;
 
         while (s < e)
               {
-              if (*s is '"' || *s is '/' || *s is '\\')
-                 {
-                 escaped = true;
-                 emit (t [0 .. s - t]);
-                 emit ("\\");
-                 t = s;
-                 }
-              ++s;           
+              switch (*s)
+                     {
+                     case '"':
+                     case '/':
+                     case '\\':
+                          patch[1] = *s;
+                          break;
+                     case '\r':
+                          patch[1] = 'r';
+                          break;
+                     case '\n':
+                          patch[1] = 'n';
+                          break;
+                     case '\t':
+                          patch[1] = 't';
+                          break;
+                     case '\b':
+                          patch[1] = 'b';
+                          break;
+                     case '\f':
+                          patch[1] = 'f';
+                          break;
+                     default:
+                          ++s;
+                          continue;
+                     }
+              emit (t [0 .. s - t]);
+              emit (patch);
+              t = ++s;
               }
 
         // did we change anything? Copy tail also
-        if (escaped)
-            emit (t [0 .. e - t]);
+        if (t is src.ptr)
+            emit (src);
         else
-           emit (src);
+           emit (t [0 .. e - t]);
 }
 
 
@@ -354,13 +257,14 @@ debug (JsonEscape)
         {
                 escape ("abc");
                 assert (escape ("abc") == "abc");
-                assert (escape ("/abc") == "\\/abc", escape ("/abc"));
-                assert (escape ("ab\\c") == "ab\\\\c", escape ("ab\\c"));
-                assert (escape ("abc\"") == "abc\\\"");
-                assert (escape ("abc/") == "abc\\/");
+                assert (escape ("/abc") == `\/abc`, escape ("/abc"));
+                assert (escape ("ab\\c") == `ab\\c`, escape ("ab\\c"));
+                assert (escape ("abc\"") == `abc\"`);
+                assert (escape ("abc/") == `abc\/`);
+                assert (escape ("\n\t\r\b\f") == `\n\t\r\b\f`);
 
                 unescape ("abc");
-                unescape ("abc\\u0020x", cast(void delegate(char[])) &Stdout.stream.write);
+                unescape ("abc\\u0020x", (char[] p){Stdout(p);});
                 assert (unescape ("abc") == "abc");
                 assert (unescape ("abc\\") == "abc\\");
                 assert (unescape ("abc\\t") == "abc\t");
