62bfeac26d9f3ea837226fcf6cd509437f35b319
larsivi <larsivi@94d23b9d-ec12-0410-b399-9fdf8a1d4f26>
2010-02-08 22:45:57 +0000

    Bundle script updates

--- build/script/tango-bundle.sh
+++ build/script/tango-bundle.sh
@@ -4,6 +4,7 @@
 # Env variable BITS should be set to 64 on 64 bit platforms, otherwise nothing
 # Env variable TANGOUPLOADPW must be set to a file holding "user:pass"
 # The compiler used must be on the path
+# Currently the dmd.conf must be present at the same level as this script
 
 # For a bundle with the compiler in it, use --dmd (only compiler supported sofar)
 
@@ -18,7 +19,7 @@ do
 done
  
 # clean old
-rm -rf bundle export trunk *.o *.a
+rm -rf bundle export trunk 
 
 # check out
 svn co http://svn.dsource.org/projects/tango/trunk
@@ -31,8 +32,9 @@ mkdir -p bundle/bin
 mkdir -p bundle/lib
 mkdir -p bundle/import
 
-# copy binaries
+# copy binaries and config
 cp export/build/bin/linux$BITS/* bundle/bin
+cp dmd.conf bundle/bin/
 
 # copy imports and .txt
 cp -r export/tango bundle/import
@@ -41,26 +43,30 @@ cp export/*.txt bundle
 rm -rf bundle/import/tango/core/rt
 
 # create library
-export/build/bin/$PLATFORM$BITS/bob -u -r$DC -c$DC -p$PLATFORM export -llibtango-$DC
-cp libtango-$DC.a bundle/lib
+pushd export
+build/bin/$PLATFORM$BITS/bob -v -u -r=$DC -c=$DC -p=$PLATFORM -l=libtango-$DC . 
+cp libtango-$DC.a ../bundle/lib
+popd
 
-pushd bundle
 BUNDLE=
 # deal with compiler
-if [ $DMD == 1 ]
+if [ $DMD = 1 ]
 then
-    ../export/build/script/fetch-dmd.sh
-    # create tar.gz
-    BUNDLE=tango-bin-$PLATFORM$BITS-$DC-with-compiler.tar.gz
-    tar -czf $BUNDLE *
+    export/build/script/fetch-dmd.sh
+    # name bundle
+    DMDVER=`cat dmd.version.txt`
+    BUNDLE=tango-bin-$PLATFORM$BITS-with-$DC.$DMDVER.tar.gz
 else
-    # create tar.gz
+    # name bundle
     BUNDLE=tango-bin-$PLATFORM$BITS-$DC.tar.gz
-    tar -czf $BUNDLE * 
 fi
 
+pushd bundle
+#package
+tar -czf $BUNDLE *
+
 # upload
-curl --transfer-file $BUNDLE --user `cat $TANGOUPLOADPW` http://downloads.dsource.org/projects/tango/snapshot
+curl --upload-file $BUNDLE --user `cat $TANGOUPLOADPW` http://downloads.dsource.org/projects/tango/snapshot/
 
 popd
 
