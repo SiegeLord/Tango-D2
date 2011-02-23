9713f64b487a1cfff7f5fe536e9db2bdb244d9bb
SiegeLord <slabode.aim.com>
2011-02-22 21:06:56 -0500

    tango.core.Thread had some weird issues with Exceptions that I somehow missed last time

--- tango/core/Thread.d
+++ tango/core/Thread.d
@@ -268,7 +268,7 @@ else version( Posix )
             {
                 obj.run();
             }
-            catch( Object o )
+            catch( Throwable o )
             {
                 obj.m_unhandled = o;
             }
@@ -1399,7 +1399,7 @@ private:
         bool            m_isRunning;
     }
     bool                m_isDaemon;
-    public Object              m_unhandled;
+    public Throwable              m_unhandled;
 
 
 private:
@@ -2620,7 +2620,7 @@ private
         {
             obj.run();
         }
-        catch( Object o )
+        catch( Throwable o )
         {
             obj.m_unhandled = o;
         }
@@ -3093,7 +3093,7 @@ class Fiber
         }
         if( m_unhandled )
         {
-            Object obj  = m_unhandled;
+            Throwable obj  = m_unhandled;
             m_unhandled = null;
             if( rethrow )
                 throw obj;
@@ -3293,7 +3293,7 @@ class Fiber
      * In:
      *  obj must not be null.
      */
-    static void yieldAndThrow( Object obj )
+    static void yieldAndThrow( Throwable obj )
     in
     {
         assert( obj );
@@ -3416,7 +3416,7 @@ private:
         void delegate() m_dg;
     }
     bool                m_isRunning;
-    Object              m_unhandled;
+    Throwable              m_unhandled;
     State               m_state;
     char[]              m_name;
 public:
