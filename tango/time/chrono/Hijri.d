9c5314dd289d01e993ddbcfeb20d8862d41a30d2
SiegeLord <slabode@aim.com>
2011-03-11 20:17:37 -0500

    Port tango.time.chrono.Hijri

--- tango/time/chrono/Hijri.d
+++ tango/time/chrono/Hijri.d
@@ -22,12 +22,12 @@ private import tango.time.chrono.Calendar;
  */
 public class Hijri : Calendar {
 
-  private static const uint[] DAYS_TO_MONTH = [ 0, 30, 59, 89, 118, 148, 177, 207, 236, 266, 295, 325, 355 ];
+  private enum uint[] DAYS_TO_MONTH = [ 0, 30, 59, 89, 118, 148, 177, 207, 236, 266, 295, 325, 355 ];
 
   /**
    * Represents the current era.
    */
-  public const uint HIJRI_ERA = 1;
+  public enum uint HIJRI_ERA = 1;
 
   /**
    * Overridden. Returns a Time value set to the specified date and time in the specified _era.
@@ -42,7 +42,7 @@ public class Hijri : Calendar {
    *   era = An integer representing the _era.
    * Returns: A Time set to the specified date and time.
    */
-  public override Time toTime(uint year, uint month, uint day, uint hour, uint minute, uint second, uint millisecond, uint era) {
+  public override const Time toTime(uint year, uint month, uint day, uint hour, uint minute, uint second, uint millisecond, uint era) {
     return Time((daysSinceJan1(year, month, day) - 1) * TimeSpan.TicksPerDay + getTimeTicks(hour, minute, second)) + TimeSpan.fromMillis(millisecond);
   }
 
@@ -51,7 +51,7 @@ public class Hijri : Calendar {
    * Params: time = A Time value.
    * Returns: A DayOfWeek value representing the day of the week of time.
    */
-  public override DayOfWeek getDayOfWeek(Time time) {
+  public override const DayOfWeek getDayOfWeek(const(Time) time) {
     return cast(DayOfWeek) (cast(uint) (time.ticks / TimeSpan.TicksPerDay + 1) % 7);
   }
 
@@ -60,7 +60,7 @@ public class Hijri : Calendar {
    * Params: time = A Time value.
    * Returns: An integer representing the day of the month of time.
    */
-  public override uint getDayOfMonth(Time time) {
+  public override const uint getDayOfMonth(const(Time) time) {
     return extractPart(time.ticks, DatePart.Day);
   }
 
@@ -69,7 +69,7 @@ public class Hijri : Calendar {
    * Params: time = A Time value.
    * Returns: An integer representing the day of the year of time.
    */
-  public override uint getDayOfYear(Time time) {
+  public override const uint getDayOfYear(const(Time) time) {
     return extractPart(time.ticks, DatePart.DayOfYear);
   }
 
@@ -78,7 +78,7 @@ public class Hijri : Calendar {
    * Params: time = A Time value.
    * Returns: An integer representing the day of the year of time.
    */
-  public override uint getMonth(Time time) {
+  public override const uint getMonth(const(Time) time) {
     return extractPart(time.ticks, DatePart.Month);
   }
 
@@ -87,7 +87,7 @@ public class Hijri : Calendar {
    * Params: time = A Time value.
    * Returns: An integer representing the year in time.
    */
-  public override uint getYear(Time time) {
+  public override const uint getYear(const(Time) time) {
     return extractPart(time.ticks, DatePart.Year);
   }
 
@@ -96,7 +96,7 @@ public class Hijri : Calendar {
    * Params: time = A Time value.
    * Returns: An integer representing the ear in time.
    */
-  public override uint getEra(Time time) {
+  public override const uint getEra(const(Time) time) {
     return HIJRI_ERA;
   }
 
@@ -108,7 +108,7 @@ public class Hijri : Calendar {
    *   era = An integer representing the _era.
    * Returns: The number of days in the specified _year and _month of the specified _era.
    */
-  public override uint getDaysInMonth(uint year, uint month, uint era) {
+  public override const uint getDaysInMonth(uint year, uint month, uint era) {
     if (month == 12)
       return isLeapYear(year, CURRENT_ERA) ? 30 : 29;
     return (month % 2 == 1) ? 30 : 29;
@@ -121,7 +121,7 @@ public class Hijri : Calendar {
    *   era = An integer representing the _era.
    * Returns: The number of days in the specified _year in the specified _era.
    */
-  public override uint getDaysInYear(uint year, uint era) {
+  public override const uint getDaysInYear(uint year, uint era) {
     return isLeapYear(year, era) ? 355 : 354;
   }
 
@@ -132,7 +132,7 @@ public class Hijri : Calendar {
    *   era = An integer representing the _era.
    * Returns: The number of months in the specified _year in the specified _era.
    */
-  public override uint getMonthsInYear(uint year, uint era) {
+  public override const uint getMonthsInYear(uint year, uint era) {
     return 12;
   }
 
@@ -142,7 +142,7 @@ public class Hijri : Calendar {
    * Params: era = An integer representing the _era.
    * Returns: true is the specified _year is a leap _year; otherwise, false.
    */
-  public override bool isLeapYear(uint year, uint era) {
+  public override const bool isLeapYear(uint year, uint era) {
     return (14 + 11 * year) % 30 < 11;
   }
 
@@ -150,7 +150,7 @@ public class Hijri : Calendar {
    * $(I Property.) Overridden. Retrieves the list of eras in the current calendar.
    * Returns: An integer array representing the eras in the current calendar.
    */
-  public override uint[] eras() {
+  public override const uint[] eras() {
     auto tmp = [HIJRI_ERA];
     return tmp.dup;
   }
@@ -159,11 +159,11 @@ public class Hijri : Calendar {
    * $(I Property.) Overridden. Retrieves the identifier associated with the current calendar.
    * Returns: An integer representing the identifier of the current calendar.
    */
-  public override uint id() {
+  public override const uint id() {
     return HIJRI;
   }
 
-  private long daysToYear(uint year) {
+  private const long daysToYear(uint year) {
     int cycle = ((year - 1) / 30) * 30;
     int remaining = year - cycle - 1;
     long days = ((cycle * 10631L) / 30L) + 227013L;
@@ -174,11 +174,11 @@ public class Hijri : Calendar {
     return days;
   }
 
-  private long daysSinceJan1(uint year, uint month, uint day) {
+  private const long daysSinceJan1(uint year, uint month, uint day) {
     return cast(long)(daysToYear(year) + DAYS_TO_MONTH[month - 1] + day);
   }
 
-  private int extractPart(long ticks, DatePart part) {
+  private const int extractPart(long ticks, DatePart part) {
     long days = TimeSpan(ticks).days + 1;
     int year = cast(int)(((days - 227013) * 30) / 10631) + 1;
     long daysUpToYear = daysToYear(year);
