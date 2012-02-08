92ca4bf9ac46da63cadb7320e2edda0fe973104d
SiegeLord <slabode@aim.com>
2012-02-07 19:31:15 -0500

    Move the annoyer pragma outside the version statement. This module is deprecated Phobos dependency or not

--- tango/math/BigInt.d
+++ tango/math/BigInt.d
@@ -1,13 +1,13 @@
 module tango.math.BigInt;
 
+pragma(msg, "tango.math.BigInt is deprecated. Please use std.bigint instead.");
+
 version(NoPhobos)
 {
 	
 }
 else
 {
-	pragma(msg, "tango.math.BigInt is deprecated. Please use std.bigint instead.");
-
 	deprecated:
 
 	public import std.bigint; 
