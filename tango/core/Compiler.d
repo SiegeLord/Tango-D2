42b2921a8ac119c32b0279348e6ce10711ff1661
SiegeLord <slabode@aim.com>
2013-03-07 00:54:45 -0500

    Add tango.core.Compiler to wrap __VERSION__

---
+++ tango/core/Compiler.d
@@ -0,0 +1,16 @@
+/**
+ * Module to track the compiler version.
+ *
+ * Copyright: Copyright (C) 2013 Pavel Sountsov.  All rights reserved.
+ * License:   BSD style: $(LICENSE)
+ * Authors:   Pavel Sountsov
+ */
+
+version(SDC)
+{
+	enum DMDFE_Version = 0;
+}
+else
+{
+	enum DMDFE_Version = __VERSION__;
+}
